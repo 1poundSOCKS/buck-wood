986a629813c58feab9eccdc0172ceb1bb2f0262e
1poundSOCKS <mathew.coburn@gmail.com>
2022-09-09 21:55:37 +0100

    Basic window render working

--- main.cpp
+++ main.cpp
@@ -9,87 +9,138 @@
 #pragma comment(lib,"user32.lib")
 #pragma comment(lib,"d2d1")
 
-struct app_globals {
+struct app_globals
+{
+  bool terminating;
   HINSTANCE inst;
+  int cmdShow;
   HWND wnd;
   ID2D1Factory* d2d_factory;
   ID2D1HwndRenderTarget* d2d_rendertarget;
 };
 
+struct render_state
+{
+  bool valid;
+  ID2D1SolidColorBrush* brush;
+};
+
 LPWSTR lpszWndClass = L"cpp_test";
 LPWSTR lpszTitle = L"cpp_test";
 
-app_globals InitApp(HINSTANCE,int);
-void DeinitApp(app_globals);
+app_globals *InitApp(HINSTANCE,int);
+void DeinitApp(app_globals*);
+render_state InitRenderState(const app_globals*);
+void CleanRenderState(const render_state&);
 ATOM MyRegisterClass(HINSTANCE hInstance);
-HWND InitInstance(HINSTANCE, int);
+void InitInstance(app_globals*);
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+bool ProcessMessage(MSG*);
+void DoRender(app_globals*);
 
 #define SafeRelease(i) if( i ) i->Release()
 
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance,_In_opt_ HINSTANCE hPrevInstance,_In_ LPWSTR    lpCmdLine,_In_ int       nCmdShow)
 {
-  app_globals ag = InitApp(hInstance, nCmdShow);
+  app_globals *ag = InitApp(hInstance, nCmdShow);
 
-  ag.d2d_rendertarget->BeginDraw();
-
-	ag.d2d_rendertarget->Clear(D2D1::ColorF(D2D1::ColorF::Black));
-
-	ID2D1SolidColorBrush* brush = NULL;
-  HRESULT hr = ag.d2d_rendertarget->CreateSolidColorBrush(D2D1::ColorF(D2D1::ColorF(0.0f, 0.0f, 0.5f, 1.0f)), &brush);
-  if( SUCCEEDED(hr) )
+  MSG msg;
+	while (ProcessMessage(&msg))
   {
-		D2D1_RECT_F rectangle = D2D1::RectF(0, 0, 100, 100);
-    ag.d2d_rendertarget->FillRectangle(&rectangle, brush);
-    SafeRelease(brush);
-  }
-
-  ag.d2d_rendertarget->EndDraw();
-
-	MSG msg;
-	while (GetMessage(&msg, nullptr, 0, 0))
-  {
-		if (!TranslateAccelerator(msg.hwnd, NULL, &msg))
-		{
-			TranslateMessage(&msg);
-			DispatchMessage(&msg);
-		}
+    if( ag->terminating ) continue;
+    DoRender(ag);
 	}
 
   DeinitApp(ag);
+  delete ag;
 
   return (int) msg.wParam;
 }
 
-app_globals InitApp(HINSTANCE instance, int nCmdShow)
+void DoRender(app_globals* ag)
 {
-  app_globals ag;
-  ::ZeroMemory(&ag, sizeof(app_globals));
-  ag.inst = instance;
+  if( ag->d2d_rendertarget == NULL ) return;
 
-  MyRegisterClass(ag.inst);
+  render_state rs = InitRenderState(ag);
 
-	ag.wnd = InitInstance(ag.inst, nCmdShow);
-	if( !ag.wnd ) return ag;
+  if( !rs.valid )
+  {
+    CleanRenderState(rs);
+    ::PostQuitMessage(0);
+    ag->terminating = true;
+    return;
+  }
 
-	HRESULT hr = D2D1CreateFactory(D2D1_FACTORY_TYPE_SINGLE_THREADED,&ag.d2d_factory);
+  ag->d2d_rendertarget->BeginDraw();
+  ag->d2d_rendertarget->Clear(D2D1::ColorF(D2D1::ColorF::Black));
+  D2D1_RECT_F rectangle = D2D1::RectF(0, 0, 100, 100);
+  ag->d2d_rendertarget->FillRectangle(&rectangle, rs.brush);
+  ag->d2d_rendertarget->EndDraw();
 
-  if( FAILED(hr) ) return ag;
+  CleanRenderState(rs);
+}
+
+render_state InitRenderState(const app_globals *ag)
+{
+  render_state rs;
+  ::ZeroMemory(&rs,sizeof(render_state));
+
+  HRESULT hr = ag->d2d_rendertarget->CreateSolidColorBrush(D2D1::ColorF(D2D1::ColorF(0.0f, 0.0f, 0.5f, 1.0f)), &rs.brush);
+  if( FAILED(hr) ) return rs;
+
+  rs.valid = true;
+  return rs;
+}
+
+void CleanRenderState(const render_state& rs)
+{
+  SafeRelease(rs.brush);
+}
+
+app_globals* InitApp(HINSTANCE instance, int nCmdShow)
+{
+  app_globals *ag = new app_globals;
+  ::ZeroMemory(ag, sizeof(app_globals));
+  ag->inst = instance;
+  ag->cmdShow = nCmdShow;
+
+  MyRegisterClass(ag->inst);
+
+	InitInstance(ag);
+	if( !ag->wnd ) return ag;
 
 	RECT rc;
-	GetClientRect(ag.wnd, &rc);
+	GetClientRect(ag->wnd, &rc);
 
-	hr = ag.d2d_factory->CreateHwndRenderTarget(D2D1::RenderTargetProperties(),
-    D2D1::HwndRenderTargetProperties(ag.wnd,D2D1::SizeU(rc.right - rc.left,rc.bottom - rc.top)),
-	  &ag.d2d_rendertarget);
+	HRESULT hr = D2D1CreateFactory(D2D1_FACTORY_TYPE_SINGLE_THREADED,&ag->d2d_factory);
+  if( FAILED(hr) ) return ag;
+
+	hr = ag->d2d_factory->CreateHwndRenderTarget(D2D1::RenderTargetProperties(),
+    D2D1::HwndRenderTargetProperties(ag->wnd,D2D1::SizeU(rc.right - rc.left,rc.bottom - rc.top)),&ag->d2d_rendertarget);
+  if( FAILED(hr) ) return ag;
 
   return ag;
 }
 
-void DeinitApp(app_globals ag)
+void DeinitApp(app_globals *ag)
 {
-  SafeRelease(ag.d2d_rendertarget);
-  SafeRelease(ag.d2d_factory);
+  SafeRelease(ag->d2d_rendertarget);
+  SafeRelease(ag->d2d_factory);
+}
+
+bool ProcessMessage(MSG* msg)
+{
+	if (PeekMessage(msg, nullptr, 0, 0, PM_REMOVE))
+  {
+    if (!TranslateAccelerator(msg->hwnd, NULL, msg))
+    {
+      TranslateMessage(msg);
+      DispatchMessage(msg);
+    }
+    return (msg->message != WM_QUIT);
+	}
+
+  return true;
 }
 
 ATOM MyRegisterClass(HINSTANCE hInstance)
@@ -113,62 +164,62 @@ ATOM MyRegisterClass(HINSTANCE hInstance)
     return RegisterClassExW(&wcex);
 }
 
-HWND InitInstance(HINSTANCE hInstance, int nCmdShow)
+void InitInstance(app_globals* ag)
 {
-   HWND hWnd = CreateWindowW(lpszWndClass, lpszTitle, WS_OVERLAPPEDWINDOW,
-      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
+   ag->wnd = CreateWindowW(lpszWndClass, lpszTitle, WS_OVERLAPPEDWINDOW,
+      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, ag->inst, ag);
 
-   if (!hWnd)
-   {
-      return NULL;
-   }
+   if (!ag->wnd) return;
 
-   ShowWindow(hWnd, nCmdShow);
-   UpdateWindow(hWnd);
-
-   return hWnd;
+   ShowWindow(ag->wnd, ag->cmdShow);
+   UpdateWindow(ag->wnd);
 }
 
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
-	switch (message)
-	{
-	case WM_CREATE:
-		break;
-
-	case WM_COMMAND:
-	{
-		return DefWindowProc(hWnd, message, wParam, lParam);
-	}
-	break;
-
-	case WM_SIZE:
-	{
-		UINT width = LOWORD(lParam);
-		UINT height = HIWORD(lParam);
-	}
-	break;
-
-	case WM_DISPLAYCHANGE:
-		InvalidateRect(hWnd, NULL, FALSE);
-		break;
-
-	case WM_PAINT:
-	{
+  if( message == WM_PAINT )
+  {
+    app_globals *ag = reinterpret_cast<app_globals *>(static_cast<LONG_PTR>(::GetWindowLongPtrW(hWnd,GWLP_USERDATA)));
+    // if( ag->wnd == hWnd )
+    // {
+    DoRender(ag);
+    // ValidateRect(hWnd, NULL);
+    //   return true;
+    // }
 		ValidateRect(hWnd, NULL);
+    // break;
+    return 0;//DefWindowProc(hWnd, message, wParam, lParam);
 	}
-	break;
 
-	case WM_KEYDOWN:
-		switch (wParam)
-		{
-    }
-    break;
-
-  case WM_DESTROY:
-    PostQuitMessage(0);
-    break;
+  if( message == WM_SIZE )
+  {
+    app_globals *ag = reinterpret_cast<app_globals *>(static_cast<LONG_PTR>(::GetWindowLongPtrW(hWnd,GWLP_USERDATA)));
+    UINT width = LOWORD(lParam);
+    UINT height = HIWORD(lParam);
+    if( ag->d2d_rendertarget ) ag->d2d_rendertarget->Resize(D2D1::SizeU(width, height));
+    return 0;
   }
-  
-  return DefWindowProc(hWnd, message, wParam, lParam);
+
+  if( message == WM_DISPLAYCHANGE )
+  {
+    InvalidateRect(hWnd, NULL, FALSE);
+    return 0;
+  }
+
+  if( message == WM_CREATE )
+  {
+    LPCREATESTRUCT pcs = NULL;
+    app_globals *ag = NULL;
+    pcs = (LPCREATESTRUCT)lParam;
+    ag = reinterpret_cast<app_globals*>(pcs->lpCreateParams);
+    ::SetWindowLongPtrW(hWnd,GWLP_USERDATA,reinterpret_cast<LONG_PTR>(ag));
+    return DefWindowProc(hWnd, message, wParam, lParam);
+  }
+
+  if( message == WM_DESTROY )
+  {
+    PostQuitMessage(0);
+  }
+
+	return DefWindowProc(hWnd, message, wParam, lParam);
 }
