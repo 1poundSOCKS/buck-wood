338a47437ecd740c1904c461b6f110f8d354e6eb
1poundSOCKS <mathew.coburn@gmail.com>
2022-10-23 08:59:08 +0100

    Levels load from json files

--- source/state_objects.cpp
+++ source/state_objects.cpp
@@ -1,6 +1,9 @@
 #include "state_objects.h"
 #include "data_files.h"
 
+game_level_data_ptr LoadGameLevelData(const std::wstring& dataPath, const std::wstring& file);
+std::wstring GetFullLevelFilename(const std::wstring& dataPath, const std::wstring& file);
+
 play_state::play_state()
 {
 }
@@ -9,16 +12,24 @@ game_state::game_state(const std::wstring& dataPath)
 {
   gameData = std::make_shared<game_data>();
 
-  fs::path jsonFilename = dataPath;
-  jsonFilename /= L"levels";
-  jsonFilename /= "test_level.json";
-  level_data_json_ptr jsonLevelData = std::make_unique<level_data_json>(jsonFilename.c_str());
-  game_level_data_ptr gameLevelData = LoadLevelDataFromJSON(*jsonLevelData);
-
-  gameData->push_back(gameLevelData);
-  gameData->push_back(CreateFirstGameLevelData());
-  gameData->push_back(CreateSecondGameLevelData());
-  gameData->push_back(CreateThirdGameLevelData());
+  gameData->push_back(LoadGameLevelData(dataPath, L"level_001.json"));
+  gameData->push_back(LoadGameLevelData(dataPath, L"level_002.json"));
+  gameData->push_back(LoadGameLevelData(dataPath, L"level_003.json"));
+  gameData->push_back(LoadGameLevelData(dataPath, L"level_004.json"));
 
   playState = std::make_unique<play_state>();
+}
+
+game_level_data_ptr LoadGameLevelData(const std::wstring& dataPath, const std::wstring& file)
+{
+  level_data_json_ptr jsonLevelData = std::make_unique<level_data_json>(GetFullLevelFilename(dataPath, file).c_str());
+  return LoadLevelDataFromJSON(*jsonLevelData);
+}
+
+std::wstring GetFullLevelFilename(const std::wstring& dataPath, const std::wstring& file)
+{
+  fs::path fullFilename = dataPath;
+  fullFilename /= L"levels";
+  fullFilename /= file;
+  return fullFilename;
 }
