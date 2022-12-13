c1d3f5bab6d999c0b8da428e72ee246ab604cacd
1poundSOCKS <mathew.coburn@gmail.com>
2022-12-13 20:10:34 +0000

    Refactor play screen render

--- source/screens/play/play_screen_render.cpp
+++ source/screens/play/play_screen_render.cpp
@@ -5,6 +5,8 @@
 #include "screen_render_data.h"
 #include "level_render.h"
 
+void RenderScreenStateText(ID2D1RenderTarget* renderTarget, const play_screen_state& screenState, const bespoke_render_data& renderData);
+void RenderLevelTimer(ID2D1RenderTarget* renderTarget, const play_screen_state& screenState, const bespoke_render_data& renderData);
 std::wstring GetGameCompleteMsg(const std::vector<float>& levelTimes);
 
 void RenderFrame(
@@ -12,45 +14,86 @@ void RenderFrame(
   const bespoke_render_data& renderData,
   const play_screen_state& screenState)
 {
-  const auto renderBrushSelector = screen_render_brush_selector { renderData.renderBrushes };
-  const auto textFormatSelector = screen_render_text_format_selector { renderData.textFormats };
-
   renderTarget->Clear(D2D1::ColorF(D2D1::ColorF::Black));
 
   RenderLevel(renderTarget, renderData, *screenState.levelState);
 
   renderTarget->SetTransform(D2D1::Matrix3x2F::Identity());
 
+  RenderScreenStateText(renderTarget, screenState, renderData);
+
+  RenderLevelTimer(renderTarget, screenState, renderData);
+  
+  const auto renderBrushSelector = screen_render_brush_selector { renderData.renderBrushes };
+  const auto textFormatSelector = screen_render_text_format_selector { renderData.textFormats };
+  RenderMouseCursor(renderTarget, renderBrushSelector[white], screenState.renderTargetMouseData.x, screenState.renderTargetMouseData.y);
+}
+
+void RenderScreenStateText(ID2D1RenderTarget* renderTarget, const play_screen_state& screenState, const bespoke_render_data& renderData)
+{
+  const auto renderBrushSelector = screen_render_brush_selector { renderData.renderBrushes };
+  const auto textFormatSelector = screen_render_text_format_selector { renderData.textFormats };
+
   switch( screenState.state )
   {
     case play_screen_state::state_paused:
-      RenderText(renderTarget, renderBrushSelector[cyan], textFormatSelector[srtf_play_screen_state], L"PAUSED", DWRITE_PARAGRAPH_ALIGNMENT_CENTER, DWRITE_TEXT_ALIGNMENT_CENTER);
+      RenderText(
+        renderTarget, 
+        renderBrushSelector[cyan], 
+        textFormatSelector[srtf_play_screen_state], 
+        L"PAUSED", 
+        DWRITE_PARAGRAPH_ALIGNMENT_CENTER, 
+        DWRITE_TEXT_ALIGNMENT_CENTER);
       break;
     case play_screen_state::state_level_complete:
-      RenderText(renderTarget, renderBrushSelector[cyan], textFormatSelector[srtf_play_screen_state], L"LEVEL COMPLETE", DWRITE_PARAGRAPH_ALIGNMENT_CENTER, DWRITE_TEXT_ALIGNMENT_CENTER);
+      RenderText(
+        renderTarget, 
+        renderBrushSelector[cyan], 
+        textFormatSelector[srtf_play_screen_state], 
+        L"LEVEL COMPLETE", 
+        DWRITE_PARAGRAPH_ALIGNMENT_CENTER, 
+        DWRITE_TEXT_ALIGNMENT_CENTER);
       break;
     case play_screen_state::state_game_complete:
-      RenderText(renderTarget, renderBrushSelector[cyan], textFormatSelector[srtf_play_screen_state], GetGameCompleteMsg(screenState.levelTimes), DWRITE_PARAGRAPH_ALIGNMENT_CENTER, DWRITE_TEXT_ALIGNMENT_CENTER);
+      RenderText(
+        renderTarget, 
+        renderBrushSelector[cyan], 
+        textFormatSelector[srtf_play_screen_state], 
+        GetGameCompleteMsg(screenState.levelTimes), 
+        DWRITE_PARAGRAPH_ALIGNMENT_CENTER, 
+        DWRITE_TEXT_ALIGNMENT_CENTER);
       break;
     case play_screen_state::state_player_dead:
-      RenderText(renderTarget, renderBrushSelector[cyan], textFormatSelector[srtf_play_screen_state], L"DEAD", DWRITE_PARAGRAPH_ALIGNMENT_CENTER, DWRITE_TEXT_ALIGNMENT_CENTER);
+      RenderText(
+        renderTarget, 
+        renderBrushSelector[cyan], 
+        textFormatSelector[srtf_play_screen_state], 
+        L"DEAD", 
+        DWRITE_PARAGRAPH_ALIGNMENT_CENTER, 
+        DWRITE_TEXT_ALIGNMENT_CENTER);
       break;
   }
+}
+
+void RenderLevelTimer(ID2D1RenderTarget* renderTarget, const play_screen_state& screenState, const bespoke_render_data& renderData)
+{
+  const auto renderBrushSelector = screen_render_brush_selector { renderData.renderBrushes };
+  const auto textFormatSelector = screen_render_text_format_selector { renderData.textFormats };
 
   float levelTimeRemaining = GetPlayTimeRemainingInSeconds(*screenState.levelState);
-  static wchar_t timerText[64];
-  swprintf(timerText, L"%.2f", levelTimeRemaining);
-  RenderText(renderTarget, renderBrushSelector[yellow], textFormatSelector[srtf_play_screen_timer], timerText, DWRITE_PARAGRAPH_ALIGNMENT_NEAR, DWRITE_TEXT_ALIGNMENT_TRAILING);
-  
-  RenderMouseCursor(renderTarget, renderBrushSelector[white], screenState.renderTargetMouseData.x, screenState.renderTargetMouseData.y);
+  std::wstring timerText = std::format(L"{:.2f}", levelTimeRemaining);
+  RenderText(
+    renderTarget, 
+    renderBrushSelector[yellow], 
+    textFormatSelector[srtf_play_screen_timer], 
+    timerText, 
+    DWRITE_PARAGRAPH_ALIGNMENT_NEAR, 
+    DWRITE_TEXT_ALIGNMENT_TRAILING);
 }
 
 std::wstring GetGameCompleteMsg(const std::vector<float>& levelTimes)
 {
   std::wstring msg;
-  for( auto levelTime: levelTimes )
-  {
-    msg += std::format(L"{:.2f}", levelTime);
-  }
+  for( auto levelTime: levelTimes ) { msg += std::format(L"{:.2f}", levelTime); }
   return msg;
 }
