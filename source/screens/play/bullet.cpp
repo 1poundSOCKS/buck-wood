bd8dbe20b6f2d5d1d00ce594274c36cbe206fc4c
1poundSOCKS <mathew.coburn@gmail.com>
2023-02-25 09:55:07 +0000

    Adding bullets to level objects collection

---
+++ source/screens/play/bullet.cpp
@@ -0,0 +1,42 @@
+#include "pch.h"
+#include "bullet.h"
+
+const float gameSpeedMultiplier = 2.0f;
+
+constexpr D2D1_RECT_F GetBulletRect()
+{
+  return { -4, -4, 4, 4 };
+}
+
+bullet::bullet(screen_render_brush_selector brushes)
+{
+  brush.attach(brushes[green]);
+  brush->AddRef();
+}
+
+auto bullet::Update(int64_t tickFrequency, int64_t tickCount) -> void
+{
+  auto updateInterval = static_cast<float>(tickCount) / static_cast<float>(tickFrequency) * gameSpeedMultiplier;
+  xPos += ( xVelocity * updateInterval );
+  yPos += ( yVelocity * updateInterval );
+}
+
+[[nodiscard]] auto bullet::HasCollided(float x, float y) const -> bool
+{
+  return false;
+}
+
+auto bullet::HitByBullet() -> void
+{
+}
+
+[[nodiscard]] auto bullet::LevelIsComplete() const -> bool
+{
+  return true;
+}
+
+void bullet::RenderTo(ID2D1RenderTarget* renderTarget, D2D1_RECT_F viewRect) const
+{
+  const D2D1_RECT_F rect = GetBulletRect();
+  renderTarget->FillRectangle(D2D1_RECT_F { rect.left + xPos, rect.top + yPos, rect.right + xPos, rect.bottom + yPos }, brush.get());
+}
--- source/screens/play/bullet.h
+++ source/screens/play/bullet.h
@@ -1,14 +1,26 @@
 #ifndef _bullet_
 #define _bullet_
 
+#include "screen_render_data.h"
+
 struct bullet
 {
+  bullet() {}
+  bullet(screen_render_brush_selector brushes);
+
+  auto Update(int64_t tickFrequency, int64_t tickCount) -> void;
+  [[nodiscard]] auto HasCollided(float x, float y) const -> bool;
+  auto HitByBullet() -> void;
+  [[nodiscard]] auto LevelIsComplete() const -> bool;
+  void RenderTo(ID2D1RenderTarget* renderTarget, D2D1_RECT_F viewRect) const;
+
   bool free = true;
   float startX = 0, startY = 0;
   float xPos = 0, yPos = 0;
   float xVelocity = 0, yVelocity = 0;
   float angle = 0;
   float range = 1000;
+  winrt::com_ptr<ID2D1SolidColorBrush> brush;
 };
 
 #endif
