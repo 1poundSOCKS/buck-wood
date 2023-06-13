e83f6a6d975be920fea2fbcd6c44554c5df01b47
1poundSOCKS <mathew.coburn@gmail.com>
2023-06-13 11:38:56 +0100

    Tidy up

--- source/framework/screen_runner.h
+++ source/framework/screen_runner.h
@@ -12,27 +12,6 @@
 
 inline bool g_closeAllScreens = false;
 
-// inline auto GetPercentageTime(int64_t frameTicks, int64_t elapsedTime) -> float
-// {
-//   return static_cast<float>(elapsedTime) / static_cast<float>(frameTicks) * 100.0f;
-// }
-
-struct screen_runner_data
-{
-  winrt::com_ptr<IDXGISwapChain> swapChain;
-  winrt::com_ptr<ID2D1RenderTarget> renderTarget;
-  winrt::com_ptr<IDWriteFactory> dwriteFactory;
-  winrt::com_ptr<IDirectInputDevice8> keyboard;
-  const window_data& windowData;
-  int fps;
-};
-
-// struct screen_diagnostics_render_data
-// {
-//   winrt::com_ptr<ID2D1SolidColorBrush> brush;
-//   winrt::com_ptr<IDWriteTextFormat> textFormat;
-// };
-
 template <typename screen_state_type> auto KeepScreenOpen(const screen_state_type& screenState) -> bool
 {
   MSG msg;
@@ -56,41 +35,21 @@ template <typename screen_state_type> auto KeepScreenOpen(const screen_state_typ
 
 template <typename screen_state_type> void OpenScreen()
 {
-  screen_runner_data data
-  {
-    framework::swapChain(),
-    framework::renderTarget(), 
-    dwrite_factory::get(),
-    framework::keyboard(), 
-    framework::windowData(),
-    framework::fps()
-  };
-
-  screen_diagnostics_render_data diagnosticsRenderData
-  {
-    CreateScreenRenderBrush(data.renderTarget.get(), D2D1::ColorF(0.5f, 0.5f, 0.5f, 1.0f)),    
-    CreateScreenRenderTextFormat(data.dwriteFactory.get(), L"Verdana", DWRITE_FONT_WEIGHT_LIGHT, DWRITE_FONT_STYLE_NORMAL, DWRITE_FONT_STRETCH_NORMAL, 20)
-  };
-
   screen_state_type screenState;
   screen_input_state inputState;
-  performance::frame_data frameData;
 
   auto previousTime = performance_counter::QueryValue();
   auto currentTime = previousTime;
   
   while( KeepScreenOpen(screenState) )
   {
-    performance::UpdateFrameData(frameData);
+    inputState.windowData = framework::windowData();
+    inputState.renderTargetMouseData = GetRenderTargetMouseData(inputState.windowData, framework::renderTarget().get());
 
-    inputState.windowData = data.windowData;
-    inputState.renderTargetMouseData = GetRenderTargetMouseData(inputState.windowData, data.renderTarget.get());
+    ReadKeyboardState(framework::keyboard().get(), inputState.keyboardState);
 
-    ReadKeyboardState(data.keyboard.get(), inputState.keyboardState);
-
-    // UpdateScreen(data, screenState, inputState, currentTime - previousTime, frameData, diagnosticsRenderData, framework::isFrameRateUnlocked());
     auto timerFrequency = performance_counter::QueryFrequency();
-    auto frameTime = timerFrequency / data.fps;
+    auto frameTime = timerFrequency / framework::fps();
 
     screenState.Refresh(inputState, framework::isFrameRateUnlocked() ? currentTime - previousTime : frameTime);
 
@@ -102,51 +61,5 @@ template <typename screen_state_type> void OpenScreen()
     inputState.previousRenderTargetMouseData = inputState.renderTargetMouseData;
   }
 }
-
-// void UpdateScreen(screen_runner_data& data, auto& screenState, const screen_input_state& inputState, int64_t frameInterval, 
-//   const performance::frame_data& frameData, screen_diagnostics_render_data& diagnosticsRenderData, bool unlockFrameRate)
-// {
-//   std::vector<std::wstring> diagnosticsData;
-//   diagnosticsData.reserve(50);
-
-//   auto timerFrequency = performance_counter::QueryFrequency();
-//   auto frameTime = timerFrequency / data.fps;
-  
-//   auto startUpdateTime = performance_counter::QueryValue();
-//   screenState.Update(inputState, unlockFrameRate ? frameInterval : frameTime);
-//   auto endUpdateTime = performance_counter::QueryValue();
-
-//   diagnosticsData.emplace_back(std::format(L"update state time: {:.1f}", GetPercentageTime(frameTime, endUpdateTime - startUpdateTime)));
-
-//   FormatDiagnostics(inputState, std::back_inserter(diagnosticsData));
-//   diagnosticsData.emplace_back(std::format(L"fps: {}", performance::GetFPS(frameData)));
-//   screenState.FormatDiagnostics(std::back_inserter(diagnosticsData));
-
-//   if( inputState.keyboardState.data[DIK_F12] & 0x80 && !(inputState.previousKeyboardState.data[DIK_F12] & 0x80) )
-//   {
-//     BOOL fullScreen = FALSE;
-//     data.swapChain->GetFullscreenState(&fullScreen, nullptr);
-//     data.swapChain->SetFullscreenState(fullScreen ? FALSE : TRUE, nullptr);
-//   }
-
-//   {
-//     render_guard renderGuard(data.renderTarget);
-
-//     auto startRenderTime = performance_counter::QueryValue();
-//     screenState.Render();
-//     auto endRenderTime = performance_counter::QueryValue();
-
-//     diagnosticsData.emplace_back(std::format(L"render time: {:.1f}", GetPercentageTime(frameTime, endRenderTime - startRenderTime)));
-    
-//     data.renderTarget->SetTransform(D2D1::IdentityMatrix());
-
-//     RenderText(data.renderTarget.get(), diagnosticsRenderData.brush.get(), diagnosticsRenderData.textFormat.get(), 
-//       GetDiagnosticsString(diagnosticsData.cbegin(), diagnosticsData.cend()));
-//   }
-
-//   data.swapChain->Present(unlockFrameRate ? 0 : 1, 0);
-
-//   screenState.PostPresent();
-// }
 
 #endif
