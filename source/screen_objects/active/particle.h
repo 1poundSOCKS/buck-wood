85aeb1fbf127a1ec0048304647b390fff39b6769
mathew coburn <mathew.coburn@gmail.com>
2023-06-23 17:54:26 +0100

    Added a generic particle class

---
+++ source/screen_objects/active/particle.h
@@ -0,0 +1,50 @@
+#pragma once
+
+#include "moving_body.h"
+
+class particle
+{
+
+public:
+
+  particle() = default;
+  particle(const game_point& position, const game_velocity& velocity, float lifespan);
+
+  auto Update(float interval) -> void;
+
+  [[nodiscard]] auto Age() const -> float;
+  [[nodiscard]] auto Destroyed() const -> bool;
+
+private:
+
+  game_point m_startPosition;
+  moving_body m_movingBody;
+  float m_lifespan { 0 };
+  float m_age { 0 };
+  bool m_destroyed { false };
+
+};
+
+inline particle::particle(const game_point& position, const game_velocity& velocity, float lifespan) :
+  m_startPosition { position }, m_movingBody { position, velocity }, m_lifespan { lifespan }
+{
+}
+
+inline auto particle::Update(float interval) -> void
+{
+  m_movingBody.Update(interval);
+
+  m_age += interval;
+
+  m_destroyed = m_age >= m_lifespan ? true : false;
+}
+
+[[nodiscard]] inline auto particle::Age() const -> float
+{
+  return m_age;
+}
+
+[[nodiscard]] inline auto particle::Destroyed() const -> bool
+{
+  return m_destroyed;
+}
