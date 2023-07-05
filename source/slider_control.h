a3fb6a72717700349cbdba1946eb93782c7340b4
1poundSOCKS <mathew.coburn@gmail.com>
2023-07-05 09:29:38 +0100

    Slider control updated

--- source/slider_control.h
+++ source/slider_control.h
@@ -5,13 +5,19 @@ class slider_control
 
 public:
 
-  slider_control(const D2D1_RECT_F& rect);
+  enum class alignment { horizontal, vertical };
+
+  slider_control(const D2D1_RECT_F& rect );
 
   [[nodiscard]] auto GetBoundingRect() const -> const D2D1_RECT_F&;
   [[nodiscard]] auto GetSliderRect(float percentage) const -> D2D1_RECT_F;
 
 private:
 
+  [[nodiscard]] auto GetSliderTop(float percentage) const -> float;
+  [[nodiscard]] auto GetSliderRight(float percentage) const -> float;
+
+  alignment m_alignment { alignment::vertical };
   D2D1_RECT_F m_rect { 0, 0, 0, 0 };
 
 };
@@ -27,6 +33,25 @@ inline slider_control::slider_control(const D2D1_RECT_F& rect) : m_rect { rect }
 
 [[nodiscard]] auto slider_control::GetSliderRect(float percentage) const -> D2D1_RECT_F
 {
-  auto right = ( m_rect.right - m_rect.left ) * percentage / 100.0f + m_rect.left;
-  return { m_rect.left, m_rect.top, right, m_rect.bottom };
+  switch( m_alignment )
+  {
+  case alignment::vertical:
+    return { m_rect.left, GetSliderTop(percentage), m_rect.right, m_rect.bottom };
+    
+  case alignment::horizontal:
+    return { m_rect.left, m_rect.top, GetSliderRight(percentage), m_rect.bottom };
+
+  default:
+    return m_rect;
+  }
+}
+
+[[nodiscard]] inline auto slider_control::GetSliderTop(float percentage) const -> float
+{
+  return m_rect.bottom - ( m_rect.bottom - m_rect.top ) * percentage / 100.0f;
+}
+
+[[nodiscard]] inline auto slider_control::GetSliderRight(float percentage) const -> float
+{
+  return m_rect.left + ( m_rect.right - m_rect.left ) * percentage / 100.0f;
 }
