c2ca9eaa65c260fcc9a7236d6e5fb04e2967864f
mathew coburn <mathew.coburn@gmail.com>
2023-10-28 19:37:13 +0100

    Option buttons added

--- source/options_menu_def.h
+++ source/options_menu_def.h
@@ -6,7 +6,18 @@
 [[nodiscard]] inline auto GetOptionsMenuDef(const render_target_area& area, std::function<void()> callback) -> menu_def
 {
   menu_def menuDef(area.GetRect());
+
+  menuDef.AddButtonDef({ L"Effects volume", []()
+  {
+  }});
+
+  menuDef.AddButtonDef({ L"Music volume", []()
+  {
+  }});
+
   menuDef.AddButtonDef({ L"Back", callback});
+
   menuDef.UpdateButtons();
+
   return menuDef;
 };
