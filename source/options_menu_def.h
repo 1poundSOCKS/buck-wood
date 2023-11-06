0427be8760ebdaf7a4ac4df885b330cde8016b03
mathew coburn <mathew.coburn@gmail.com>
2023-11-06 09:50:23 +0000

    updates to options menu

--- source/options_menu_def.h
+++ source/options_menu_def.h
@@ -8,8 +8,18 @@ namespace options_menu_def
   [[nodiscard]] inline auto get(const render_target_area& area, menu_controller& menuController) -> menu_def
   {
     menu_def menuDef(area.GetRect());
-    menuDef.AddButtonDef({ L"Effects volume", []() -> void {}});
-    menuDef.AddButtonDef({ L"Music volume", []() -> void {}});
+    menuDef.AddButtonDef({ L"Effects volume", [area, &menuController]() -> void
+    {
+      menu_def menuDef { area.GetRect() };
+      menuDef.AddButtonDef({ L"Back", [&menuController]() -> void { menuController.Close(); }});
+      menuController.Open(menuDef);
+    }});
+    menuDef.AddButtonDef({ L"Music volume", [area, &menuController]() -> void
+    {
+      menu_def menuDef { area.GetRect() };
+      menuDef.AddButtonDef({ L"Back", [&menuController]() -> void { menuController.Close(); }});
+      menuController.Open(menuDef);
+    }});
     menuDef.AddButtonDef({ L"Back", [&menuController]() -> void { menuController.Close(); }});
     return menuDef;
   };
