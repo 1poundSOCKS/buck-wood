80cb743fd6241e55da83086c5d52b76e2e807cf5
mathew coburn <mathew.coburn@gmail.com>
2023-11-18 09:49:39 +0000

    tidy up

--- source/framework/render_target.h
+++ source/framework/render_target.h
@@ -4,7 +4,6 @@
 #include "performance_counter.h"
 #include "frame_data.h"
 #include "render_guard.h"
-// #include "render.h"
 #include "sound.h"
 #include "sound_buffer.h"
 #include "dwrite_factory.h"
