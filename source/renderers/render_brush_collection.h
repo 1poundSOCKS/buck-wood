c5ffa89db7879d14768035438b3dda6b4934b45e
mathew coburn <mathew.coburn@gmail.com>
2023-11-19 21:38:21 +0000

    refactor in progress

--- source/renderers/render_brush_collection.h
+++ source/renderers/render_brush_collection.h
@@ -9,18 +9,31 @@ class render_brush_collection
 
 public:
 
-  using brush_initializer = std::tuple<brush_type, render_brush_def>;
+  using initializer_type = std::tuple<brush_type, render_brush_def>;
+  using collection_type = std::vector<render_brush>;
 
   render_brush_collection<brush_type>(const auto& brushes);
+
+private:
+
+  collection_type m_brushes;
 
 };
 
 template <typename brush_type>
 render_brush_collection<brush_type>::render_brush_collection(const auto& brushes)
 {
-  for( const auto& brushInitializer : brushes )
+  for( const auto& [brushIndex, brushDef] : brushes )
   {
-    const auto &[brushIndex, brushDef] = brushInitializer;
-    auto brush = brushDef.CreateBrush(render_target::get().get());
+    auto brush = brushDef.CreateBrush(render_target::renderTarget().get());
+
+    auto requiredSize = static_cast<std::underlying_type<brush_type>::type>(brushIndex) + 1;
+
+    if( m_brushes.size() < requiredSize )
+    {
+      m_brushes.resize(requiredSize);
+    }
+
+    m_brushes[brushIndex] = brush;
   }
 }
