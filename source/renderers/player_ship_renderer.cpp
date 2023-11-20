01f4078acf87386fe5aeb042c33e1b92bdb5dfcd
mathew coburn <mathew.coburn@gmail.com>
2023-11-20 18:45:51 +0000

    refactor

--- source/renderers/player_ship_renderer.cpp
+++ source/renderers/player_ship_renderer.cpp
@@ -1,53 +1,14 @@
 #include "pch.h"
 #include "player_ship_renderer.h"
 
-struct render_line
-{
-  render_line(const D2D1_POINT_2F& start, const D2D1_POINT_2F& end, ID2D1SolidColorBrush* brush, float width = 1);
-  
-  D2D1_POINT_2F start, end;
-  ID2D1SolidColorBrush* brush = nullptr;
-  float width = 1;
-};
-
-constexpr auto GetPlayerThrusterGeometryData()
+player_ship_renderer::player_ship_renderer()
 {
   constexpr float width { 5 };
-  constexpr float yOffset { 16 };
-  
-  return std::array {
-    game_point { width, yOffset },
-    game_point { -width, yOffset }
-  };
-}
+  constexpr float top { 14 };
+  constexpr float bottom { 20 };
 
-template <typename input_iterator_type, typename insert_iterator_type>
-void CreateDisconnectedRenderLines(
-  typename input_iterator_type begin, 
-  typename input_iterator_type end, 
-  insert_iterator_type insertIterator, 
-  ID2D1SolidColorBrush* brush, 
-  float width, 
-  float x=0, 
-  float y=0)
-{
-  for( auto i = begin; i != end; std::advance(i, 2) )
-  {
-    auto next = std::next(i);
-    if( next != end ) insertIterator = render_line({i->x + x, i->y + y}, {next->x + x, next->y + y}, brush, width);
-  }
-};
-
-template <typename input_iterator_type>
-void RenderLines(
-  ID2D1RenderTarget* renderTarget, 
-  input_iterator_type begin, 
-  input_iterator_type end)
-{
-  for( auto line = begin; line != end; ++line )
-  {
-    renderTarget->DrawLine(line->start, line->end, line->brush, line->width);
-  }
+  m_thrusterGeometry.Load(std::initializer_list { game_point { width, top }, game_point { -width, top },
+    game_point { -width, bottom }, game_point { width, bottom } });
 }
 
 auto player_ship_renderer::Write(const player_ship& playerShip) const -> void
@@ -57,22 +18,8 @@ auto player_ship_renderer::Write(const player_ship& playerShip) const -> void
   if( playerShip.ThrusterOn() )
   {
     const auto& position = playerShip.Position();
-    auto angle = playerShip.Angle();
-
-    std::vector<game_point> thrusterPoints;
-    GetTransformedThrusterGeometry(position.x, position.y, angle, std::back_inserter(thrusterPoints));
-
-    std::vector<render_line> renderLines;
-    auto renderLinesInserter = std::back_inserter(renderLines);
-    CreateDisconnectedRenderLines(thrusterPoints.cbegin(), thrusterPoints.cend(), renderLinesInserter, m_thrusterBrush.get(), 5);
-    RenderLines(render_target::renderTarget().get(), renderLines.cbegin(), renderLines.cend());    
+    auto thrusterTransform = D2D1::Matrix3x2F::Rotation(playerShip.Angle(), D2D1::Point2F(0,0)) * D2D1::Matrix3x2F::Translation(position.x, position.y);
+    transformed_path_geometry transforedThrusterGeometry { m_thrusterGeometry, thrusterTransform };
+    m_thrusterRenderer.Write(transforedThrusterGeometry);
   }
-}
-
-auto player_ship_renderer::GetTransformedThrusterGeometry(float x, float y, float angle, std::back_insert_iterator<points_collection> pointsInserter) const -> void
-{
-  const auto& thrusterGeometryData = GetPlayerThrusterGeometryData();
-
-  TransformPoints(thrusterGeometryData.cbegin(), thrusterGeometryData.cend(), pointsInserter, 
-    D2D1::Matrix3x2F::Rotation(angle, D2D1::Point2F(0,0)) * D2D1::Matrix3x2F::Translation(x, y));
 }
--- source/renderers/player_ship_renderer.h
+++ source/renderers/player_ship_renderer.h
@@ -12,15 +12,13 @@ public:
 
   using points_collection = std::vector<game_point>;
 
+  player_ship_renderer();
   auto Write(const player_ship& playerShip) const -> void;
 
 private:
 
-  auto GetTransformedThrusterGeometry(float x, float y, float angle, std::back_insert_iterator<points_collection> pointsInserter) const -> void;
-
-private:
-
   geometry_renderer m_renderer { screen_render_brush_dark_grey.CreateBrush(), screen_render_brush_white.CreateBrush(), 2 };
-  render_brush m_thrusterBrush { screen_render_brush_red.CreateBrush() };
+  path_geometry m_thrusterGeometry;
+  geometry_renderer m_thrusterRenderer { screen_render_brush_red.CreateBrush() };
 
 };
