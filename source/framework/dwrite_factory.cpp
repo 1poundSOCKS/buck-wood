cefe480c76c67f87e5f90b8037e7a268211c108f
mathew coburn <mathew.coburn@gmail.com>
2023-11-26 16:12:16 +0000

    refactor

--- source/framework/dwrite_factory.cpp
+++ source/framework/dwrite_factory.cpp
@@ -3,28 +3,12 @@
 #include "framework.h"
 #include "directx_functions.h"
 
-dwrite_factory* dwrite_factory::m_instance = nullptr;
-
 auto dwrite_factory::create() -> void
 {
   destroy();
   m_instance = new dwrite_factory();
 }
 
-auto dwrite_factory::destroy() -> void
-{
-  if( m_instance )
-  {
-    delete m_instance;
-    m_instance = nullptr;
-  }
-}
-
-auto dwrite_factory::get() -> const winrt::com_ptr<IDWriteFactory>&
-{
-  return m_instance->m_dwriteFactory;
-}
-
-dwrite_factory::dwrite_factory() : m_dwriteFactory { CreateDWriteFactory() }
+dwrite_factory::dwrite_factory() : com_singleton { CreateDWriteFactory() }
 {  
 }
--- source/framework/dwrite_factory.h
+++ source/framework/dwrite_factory.h
@@ -1,17 +1,15 @@
 #pragma once
 
-class dwrite_factory
+#include "com_singleton.h"
+
+class dwrite_factory : public com_singleton<IDWriteFactory>
 {
 public:
 
   static auto create() -> void;
-  static auto destroy() -> void;
-  static auto get() -> const winrt::com_ptr<IDWriteFactory>&;
 
 private:
   
-  static dwrite_factory* m_instance;
-
   dwrite_factory();
-  winrt::com_ptr<IDWriteFactory> m_dwriteFactory;
+
 };
