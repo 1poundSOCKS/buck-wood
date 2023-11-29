72063873e588a8409b5ebb74118b816fdc15c6e4
mathew coburn <mathew.coburn@gmail.com>
2023-11-29 08:41:30 +0000

    refactor demo level

--- source/demo_level.cpp
+++ source/demo_level.cpp
@@ -3,20 +3,8 @@
 
 demo_level::demo_level(const level_grid& levelGrid)
 {
-  auto leftInnerBorder = static_cast<float>(levelGrid.LeftBorder());
-  auto topInnerBorder = static_cast<float>(levelGrid.TopBorder());
-  auto rightInnerBorder = static_cast<float>(levelGrid.RightBorder());
-  auto bottomInnerBorder = static_cast<float>(levelGrid.BottomBorder());
-
-  auto blankObjectPoints = std::array
-  {
-    game_point { leftInnerBorder, topInnerBorder },
-    game_point { rightInnerBorder, topInnerBorder },
-    game_point { rightInnerBorder, bottomInnerBorder },
-    game_point { leftInnerBorder, bottomInnerBorder }
-  };
-
-  std::ranges::copy(blankObjectPoints, std::back_inserter(m_boundary));
+  geometry_builder geometryBuilder { levelGrid.LeftColumn() - 1, levelGrid.TopRow() - 1, levelGrid.ColumnWidth(), levelGrid.RowHeight() };
+  geometryBuilder.Run(m_buildCommands, std::back_inserter(m_boundary));
 }
 
 [[nodiscard]] auto demo_level::Boundary() const -> const std::vector<game_point>&
--- source/demo_level.h
+++ source/demo_level.h
@@ -3,6 +3,84 @@
 #include "geometry.h"
 #include "level_grid.h"
 
+class build_command
+{
+
+public:
+
+  enum class move_direction { none, left, right, up, down };
+
+  build_command(move_direction moveDirection, int count) : m_moveDirection { moveDirection }, m_count { count }
+  {
+  }
+
+  [[nodiscard]] auto MoveDirection() const -> move_direction
+  {
+    return m_moveDirection;
+  }
+
+  [[nodiscard]] auto Count() const -> int
+  {
+    return m_count;
+  }
+
+private:
+
+  move_direction m_moveDirection { move_direction::none };
+  int m_count { 0 };
+
+};
+
+class geometry_builder
+{
+
+public:
+
+  geometry_builder(int startX, int startY, int cellWidth, int cellHeight) : 
+    m_startX { startX }, m_startY { startY }, m_cellWidth { cellWidth }, m_cellHeight { cellHeight }
+  {
+  }
+
+  auto Run(auto&& commands, auto output) -> void
+  {
+    float x = static_cast<float>(m_startX * m_cellWidth);
+    float y = static_cast<float>(m_startY * m_cellHeight);
+
+    output = game_point { x, y };
+
+    auto view = commands | std::ranges::views::transform([this, &x,&y](const build_command& buildCommand) -> game_point
+    {
+      switch( buildCommand.MoveDirection() )
+      {
+        case build_command::move_direction::left:
+          x -= buildCommand.Count() * m_cellWidth;
+          break;
+        case build_command::move_direction::right:
+          x += buildCommand.Count() * m_cellWidth;
+          break;
+        case build_command::move_direction::up:
+          y -= buildCommand.Count() * m_cellHeight;
+          break;
+        case build_command::move_direction::down:        
+          y += buildCommand.Count() * m_cellHeight;
+          break;
+      }
+
+      return { x, y };
+    });
+
+    std::ranges::copy(view, output);
+  }
+
+private:
+
+  int m_startX { 0 };
+  int m_startY { 0 };
+  int m_cellWidth { 1 };
+  int m_cellHeight { 1 };
+
+};
+
 class demo_level
 {
 
@@ -15,5 +93,15 @@ public:
 private:
 
   std::vector<game_point> m_boundary;
+
+  inline static auto m_buildCommands = std::array {
+    build_command { build_command::move_direction::right, 4 },
+    build_command { build_command::move_direction::up, 2 },
+    build_command { build_command::move_direction::right, 2 },
+    build_command { build_command::move_direction::down, 5 },
+    build_command { build_command::move_direction::right, 4 },
+    build_command { build_command::move_direction::down, 10 },
+    build_command { build_command::move_direction::left, 10 }
+  };
 
 };
