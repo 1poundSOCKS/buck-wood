92d8d473f51318a2308906ba228b5c3740f07c70
mathew coburn <mathew.coburn@gmail.com>
2023-12-12 08:45:50 +0000

    refactor

--- source/framework/collisions.cpp
+++ source/framework/collisions.cpp
@@ -19,3 +19,39 @@
   float b = line.start.y - m * line.start.x;
   return m * x + b;
 }
+
+[[nodiscard]] auto game_closed_object::GetBoundingRect() const -> game_rect
+{
+  auto firstPoint = points.cbegin();
+  return std::reduce(
+    std::next(firstPoint), points.cend(), 
+    game_rect {firstPoint->x , firstPoint->y, firstPoint->x, firstPoint->y},
+    [](game_rect rect, game_point point) -> game_rect
+    {
+      return {
+        min(point.x, rect.topLeft.x),
+        min(point.y, rect.topLeft.y),
+        max(point.x, rect.bottomRight.x),
+        max(point.y, rect.bottomRight.y)
+      };
+    }
+  );
+}
+
+[[nodiscard]] bool DoOverlap(game_rect rect1, game_rect rect2) 
+{
+  return !(
+    rect1.bottomRight.x < rect2.topLeft.x ||
+    rect1.topLeft.x > rect2.bottomRight.x ||
+    rect1.bottomRight.y < rect2.topLeft.y ||
+    rect1.topLeft.y > rect2.bottomRight.y
+  );
+}
+
+[[nodiscard]] auto GetCentrePoint(game_rect rect) -> game_point
+{
+  return {
+    (rect.topLeft.x + rect.bottomRight.x) / 2,
+    (rect.topLeft.y + rect.bottomRight.y) / 2
+  };
+}
