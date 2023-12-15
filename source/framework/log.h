580a5f820b8aa2ebd8141f41b8cd9348532168d3
mathew coburn <mathew.coburn@gmail.com>
2023-12-15 11:44:35 +0000

    logging enhancement

--- source/framework/log.h
+++ source/framework/log.h
@@ -16,6 +16,7 @@ private:
 
   auto Open() -> void;
   template <typename...Args> auto Write(type msgType, std::format_string<Args...> fmt, Args&&... args) -> void;
+  static auto GetMessageTypeAsString(type msgType) -> const char*;
 
 private:
 
@@ -31,15 +32,30 @@ template <typename...Args> auto log::write(type msgType, std::format_string<Args
 }
 
 template <typename...Args> auto log::Write(type msgType, std::format_string<Args...> fmt, Args&&... args) -> void
-{
+{ 
   auto timestamp = std::chrono::system_clock::now() - m_creationTime;
   auto msg = std::format(fmt, std::forward<Args>(args)...);
 
-  m_file << std::format("{} {}", timestamp.count(), msg) << "\n";
+  m_file << std::format("{} {} {}", timestamp.count(), GetMessageTypeAsString(msgType), msg) << "\n";
 
   if( msgType == type::fatal )
   {
     m_file.flush();
     exit(1);
+  }
+}
+
+inline auto log::GetMessageTypeAsString(type msgType) -> const char*
+{
+  switch( msgType )
+  {
+    case type::info:
+      return "INFO";
+    case type::error:
+      return "ERROR";
+    case type::fatal:
+      return "FATAL";
+    default:
+      return "***";
   }
 }
