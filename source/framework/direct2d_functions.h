9ab7469eed9423a18eab605f28021c283d8b9450
Mathew Coburn <mathew.coburn@gmail.com>
2024-02-04 19:07:46 +0000

    Refactor

--- source/framework/direct2d_functions.h
+++ source/framework/direct2d_functions.h
@@ -42,14 +42,21 @@ namespace direct2d
     return sourceGeometry;
   }
 
-  inline auto GetDistanceBetweenPoints(D2D1_POINT_2F point1, D2D1_POINT_2F point2) -> float
+  inline [[nodiscard]] auto CreateEllipseGeometry(ID2D1Factory* d2dFactory, const D2D1_ELLIPSE& ellipse) -> winrt::com_ptr<ID2D1EllipseGeometry>
+  {
+    winrt::com_ptr<ID2D1EllipseGeometry> geometry;
+    d2dFactory->CreateEllipseGeometry(ellipse, geometry.put());
+    return geometry;
+  }
+
+  inline [[nodiscard]] auto GetDistanceBetweenPoints(D2D1_POINT_2F point1, D2D1_POINT_2F point2) -> float
   {
     float cx = point2.x - point1.x;
     float cy = point2.y - point1.y;
     return sqrt( cx * cx + cy * cy );
   }
 
-  inline auto GetRadiusFromBounds(D2D1_RECT_F bounds) -> float
+  inline [[nodiscard]] auto GetRadiusFromBounds(D2D1_RECT_F bounds) -> float
   {
     float topLeft = GetDistanceBetweenPoints({0, 0}, {bounds.left, bounds.top});
     float topRight = GetDistanceBetweenPoints({0, 0}, {bounds.right, bounds.top});
@@ -58,79 +65,79 @@ namespace direct2d
     return std::max({topLeft, topRight, bottomLeft, bottomRight});
   }
 
-  inline auto GetGeometryRadius(ID2D1Geometry* geometry) -> float
+  inline [[nodiscard]] auto GetGeometryRadius(ID2D1Geometry* geometry) -> float
   {
     D2D1_RECT_F bounds;
     return SUCCEEDED(geometry->GetBounds(NULL, &bounds)) ? GetRadiusFromBounds(bounds) : 0;
   }
 
-  inline auto GetAngleBetweenPoints(D2D1_POINT_2F startPoint, D2D1_POINT_2F endPoint) -> float
+  inline [[nodiscard]] auto GetAngleBetweenPoints(D2D1_POINT_2F startPoint, D2D1_POINT_2F endPoint) -> float
   {
     float radians = atan2(endPoint.y - startPoint.y, endPoint.x - startPoint.x);
     float degrees = RADTODEG(radians);
     return degrees + 90.0f;
   }
 
-  inline auto GetRectAtPosition(D2D1_POINT_2F position, float cx, float cy) -> D2D1_RECT_F
+  inline [[nodiscard]] auto GetRectAtPosition(D2D1_POINT_2F position, float cx, float cy) -> D2D1_RECT_F
   {
     auto halfWidth = cx / 2;
     auto halfHeight = cy / 2;
     return { position.x - halfWidth, position.y - halfHeight, position.x + halfWidth, position.y + halfHeight };
   }
 
-  inline auto ShiftPosition(D2D1_POINT_2F position, float cx, float cy) -> D2D1_POINT_2F
+  inline [[nodiscard]] auto ShiftPosition(D2D1_POINT_2F position, float cx, float cy) -> D2D1_POINT_2F
   {
     return { position.x + cx, position.y + cy  };
   }
 
-  inline auto ShiftPosition(D2D1_POINT_2F position, D2D1_POINT_2F value) -> D2D1_POINT_2F
+  inline [[nodiscard]] auto ShiftPosition(D2D1_POINT_2F position, D2D1_POINT_2F value) -> D2D1_POINT_2F
   {
     return { position.x + value.x, position.y + value.y  };
   }
 
-  inline auto CalculateVelocity(float speed, float direction) -> VELOCITY_2F
+  inline [[nodiscard]] auto CalculateVelocity(float speed, float direction) -> VELOCITY_2F
   {
     return { speed * sin(DEGTORAD(direction)), -speed * cos(DEGTORAD(direction)) };
   }
 
-  inline auto CalculatePosition(float distance, float direction) -> D2D1_POINT_2F
+  inline [[nodiscard]] auto CalculatePosition(float distance, float direction) -> D2D1_POINT_2F
   {
     return { distance * sin(DEGTORAD(direction)), -distance * cos(DEGTORAD(direction)) };
   }
 
-  inline auto CalculatePosition(D2D1_POINT_2F position, VELOCITY_2F velocity, float interval) -> D2D1_POINT_2F
+  inline [[nodiscard]] auto CalculatePosition(D2D1_POINT_2F position, VELOCITY_2F velocity, float interval) -> D2D1_POINT_2F
   {
     return { position.x + velocity.x * interval, position.y + velocity.y * interval };
   }
 
-  inline auto CombineVelocities(VELOCITY_2F velocity1, VELOCITY_2F velocity2) -> VELOCITY_2F
+  inline [[nodiscard]] auto CombineVelocities(VELOCITY_2F velocity1, VELOCITY_2F velocity2) -> VELOCITY_2F
   {
     return { velocity1.x + velocity2.x, velocity1.y + velocity2.y };
   }
 
-  inline auto MultiplyVelocity(VELOCITY_2F velocity, float amount) -> VELOCITY_2F
+  inline [[nodiscard]] auto MultiplyVelocity(VELOCITY_2F velocity, float amount) -> VELOCITY_2F
   {
     return { velocity.x * amount, velocity.y * amount };
   }
 
-  inline auto CalculateSpeed(VELOCITY_2F velocity) -> float
+  inline [[nodiscard]] auto CalculateSpeed(VELOCITY_2F velocity) -> float
   {
     return sqrt(velocity.x * velocity.x + velocity.y * velocity.y);
   }
 
-  inline auto CalculateDirection(VELOCITY_2F velocity) -> float
+  inline [[nodiscard]] auto CalculateDirection(VELOCITY_2F velocity) -> float
   {
     return direct2d::GetAngleBetweenPoints({0, 0}, {velocity.x, velocity.y});
   }
 
-  inline auto GetAngleDifference(float referenceAngle, float bearing) -> float
+  inline [[nodiscard]] auto GetAngleDifference(float referenceAngle, float bearing) -> float
   {
     auto angleDifference = bearing - referenceAngle;
     angleDifference = angleDifference > 180 ? angleDifference - 360 : angleDifference;
     return angleDifference < -180 ? angleDifference + 360 : angleDifference;
   }
 
-  inline auto RotateAngle(float angle, float rotation) -> float
+  inline [[nodiscard]] auto RotateAngle(float angle, float rotation) -> float
   {
     angle += rotation;
     
@@ -147,7 +154,7 @@ namespace direct2d
     return angle;
   }
 
-  inline auto CalculatePosition(D2D1_POINT_2F position, float angle, float distance) -> D2D1_POINT_2F
+  inline [[nodiscard]] auto CalculatePosition(D2D1_POINT_2F position, float angle, float distance) -> D2D1_POINT_2F
   {
     return ShiftPosition(position, CalculatePosition(distance, angle));
   }
