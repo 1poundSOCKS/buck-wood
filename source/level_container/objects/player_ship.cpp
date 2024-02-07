be15226f01f7fd5cfd9c608fa7d9ff5cff6b5f81
Mathew Coburn <mathew.coburn@gmail.com>
2024-02-07 09:32:02 +0000

    control updates

--- source/level_container/objects/player_ship.cpp
+++ source/level_container/objects/player_ship.cpp
@@ -9,14 +9,18 @@ auto player_ship::Update(float interval) -> void
 {
   if( m_state == state::alive )
   {
-    auto rightTriggerValue = gamepad_reader::right_trigger();
+    auto thrustControlValue = gamepad_reader::right_trigger();
+    auto shieldControlOn = gamepad_reader::button_down(XINPUT_GAMEPAD_B);
+    auto triggerControlOn = gamepad_reader::button_down(XINPUT_GAMEPAD_A);
+
     m_previousPosition = m_position;
     m_angle = m_destination ? GetUpdatedAngle(Position(), Angle(), *m_destination) : m_angle;
-    m_velocity = rightTriggerValue > 0 ? direct2d::CombineVelocities(m_velocity, direct2d::CalculateVelocity(rightTriggerValue * m_thrustPower * interval, m_angle)) : m_velocity;
+    m_velocity = thrustControlValue > 0 ? direct2d::CombineVelocities(m_velocity, direct2d::CalculateVelocity(thrustControlValue * m_thrustPower * interval, m_angle)) : m_velocity;
     m_position = GetUpdatedPosition(Position(), m_velocity, interval);
-    m_thrusterOn = rightTriggerValue ? true : false;
-    m_triggerDown = gamepad_reader::button_down(XINPUT_GAMEPAD_A);
-    m_shieldsUp = gamepad_reader::button_down(XINPUT_GAMEPAD_LEFT_SHOULDER);
+
+    m_thrusterOn = thrustControlValue > 0 ? true : false;
+    m_shieldsUp = shieldControlOn;
+    m_triggerDown = triggerControlOn;
     m_playerReloadCounter.Update(interval);
   }
 }
