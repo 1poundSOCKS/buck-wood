64fd09983735461479795a3a9b534c858ee44df8
1poundSOCKS <mathew.coburn@gmail.com>
2024-02-19 08:44:23 +0000

    game score displayed

--- source/renderers/game_score_renderer.h
+++ source/renderers/game_score_renderer.h
@@ -9,6 +9,15 @@ public:
 
   auto Write(const game_score& gameScore) const -> void
   {
+    std::wstring score = std::format(L"{}", gameScore.Value());
+    text_renderer textRenderer { m_brush, m_defaultText };
+    textRenderer.Write(m_rect, DWRITE_PARAGRAPH_ALIGNMENT_CENTER, DWRITE_TEXT_ALIGNMENT_CENTER, score.c_str());
   }
+
+private:
+
+  render_brush m_brush { screen_render_brush_green.CreateBrush() };
+  render_text_format m_defaultText { render_text_format_def { L"Courier New", DWRITE_FONT_WEIGHT_BOLD, DWRITE_FONT_STYLE_NORMAL, DWRITE_FONT_STRETCH_NORMAL, 100 }.CreateTextFormat() };
+  D2D1_RECT_F m_rect { 0, 0, 200, 200 };
 
 };
