a79f7a84b22c2b5a6d3f70975db8cb2ef02c72bf
1poundSOCKS <mathew.coburn@gmail.com>
2024-02-21 16:43:20 +0000

    refactoring...

--- source/level_container/level_container.cpp
+++ source/level_container/level_container.cpp
@@ -35,16 +35,8 @@ auto level_container::Update(float interval, D2D1_RECT_F viewRect) -> void
 
 auto level_container::UpdateObjects(float interval) -> void
 {
-  // auto playerCollisionGeometry = m_playerShip->ShieldsUp() ? static_cast<ID2D1Geometry*>(level_geometries::PlayerShieldGeometry()) : static_cast<ID2D1Geometry*>(level_geometries::PlayerShipGeometry());
-  // m_playerShip.Update(playerCollisionGeometry, interval, m_playerActive);
-
-  // dynamic_object_functions::update(m_mines, level_geometries::MineGeometry(), interval, m_playerShip->Destroyed() ? std::nullopt : std::optional<D2D1_POINT_2F>(m_playerShip->Position()));
-  // dynamic_object_functions::update(m_targets, level_geometries::TargetGeometry(), interval);
-  // dynamic_object_functions::update(m_ductFans, level_geometries::DuctFanGeometry(), interval);
   dynamic_object_functions::update(m_bullets, interval);
   dynamic_object_functions::update(m_particles, interval);
-
-  // dynamic_object_functions::update(m_defaultObjects, level_geometries::TargetGeometry(), interval);
 
   struct visitor
   {
@@ -59,7 +51,7 @@ auto level_container::UpdateObjects(float interval) -> void
     auto operator()(const player_ship& object)
     {
       m_dynamicObject.Update(level_geometries::PlayerShipGeometry(), m_interval, m_levelContainer.PlayerPosition());
-      m_levelContainer.SetPlayerPosition(object.Position());
+      m_levelContainer.UpdatePlayer(object);
     }
     auto operator()(const mine& object)
     {
@@ -90,7 +82,6 @@ auto level_container::RemoveDestroyedObjects() -> void
 {
   ValidateObjectPointers();
 
-  // dynamic_object_functions::erase_destroyed(m_mines);
   particle_functions::erase_destroyed(m_bullets);
   particle_functions::erase_destroyed(m_particles);
 
@@ -102,21 +93,12 @@ auto level_container::Render(D2D1_RECT_F viewRect) const -> void
   auto renderStart = performance_counter::QueryValue();
 
   renderer::render(m_boundary);
-  // renderer::render_all(m_asteroids);
-  // renderer::render_all(m_mines);
-  // renderer::render_all(m_targets);
-  // renderer::render_all(m_ductFans);
 
   renderer::render_all(m_staticObjects);
   renderer::render_all(m_movingObjects);
 
   renderer::render_all(m_bullets);
   renderer::render_all(m_particles);
-
-  // if( !m_playerShip->Destroyed() )
-  // {
-  //   renderer::render(m_playerShip);
-  // }
 
   auto renderEnd = performance_counter::QueryValue();
 
@@ -127,30 +109,6 @@ auto level_container::DoCollisions() -> void
 {
   level_collision_handler<level_container> collisionHandler { *this };
   
-  // if( !m_playerShip->Destroyed() )
-  // {
-  //   geometry_collision<player_ship, level_target> shipOnTargetCollision { collisionHandler };
-  //   shipOnTargetCollision(m_playerShip, m_targets);
-
-  //   geometry_collision<player_ship, mine> shipOnMineCollision { collisionHandler };
-  //   shipOnMineCollision(m_playerShip, m_mines);
-
-  //   collisionHandler.DestroyObjectOnGeometryCollision<player_ship>(m_playerShip, m_boundary, m_asteroids, m_ductFans);
-  // }
-
-  // particle_collision<level_target, bullet> bulletOnTargetCollision { collisionHandler };
-  // bulletOnTargetCollision(m_targets, m_bullets);
-
-  // particle_collision<mine, bullet> bulletOnMineCollision { collisionHandler };
-  // bulletOnMineCollision(m_mines, m_bullets);
-
-  // collision<mine> mineOnMineCollision { collisionHandler };
-  // mineOnMineCollision(m_mines);
-
-  // collisionHandler.DestroyObjectsOnGeometryCollision<mine>(m_mines, m_boundary, m_asteroids, m_ductFans);
-  // collisionHandler.DestroyParticlesOnGeometryCollision<particle>(m_particles, m_boundary, m_asteroids, m_ductFans);
-  // collisionHandler.DestroyBulletsOnGeometryCollision(m_bullets, m_boundary, m_asteroids, m_ductFans);
-
   geometry_collision<default_object, default_object> staticMovingCollisionRunner { collisionHandler };
   staticMovingCollisionRunner(m_staticObjects, m_movingObjects);
 
@@ -162,51 +120,6 @@ auto level_container::DoCollisions() -> void
   bulletCollisionRunner(m_movingObjects, m_bullets);
 
 }
-
-// auto level_container::CreateNewObjects(float interval) -> void
-// {
-//   auto damageMode = ConvertFireModeToDamageMode(m_playerShip->FireMode());
-  
-//   if( m_targettedObject && m_playerShip->CanShoot() && damageMode )
-//   {
-//     auto angleToTarget = direct2d::GetAngleBetweenPoints(m_playerShip->Position(), m_targettedObject->Position());
-//     m_bullets.emplace_back(m_playerShip->Position(), direct2d::CalculateVelocity(500, angleToTarget), *damageMode, m_targettedObject);
-//     m_playEvents.SetEvent(play_events::event_type::shot, true);
-//   }
-
-//   std::optional<D2D1_POINT_2F> playerPosition = m_playerShip->Destroyed() ? std::nullopt : std::optional<D2D1_POINT_2F>(m_playerShip->Position());
-
-//   auto shootingTargets = m_targets | std::ranges::views::filter([&playerPosition](const auto& target) -> bool { return playerPosition && target->CanShootAt(*playerPosition); });
-
-//   for( const auto& target : shootingTargets )
-//   {
-//     auto [thrust, maxSpeed, hardnessType] = m_stage.MineParameters();
-//     m_mines.emplace_back(level_geometries::MineGeometry(), target->Position(), thrust, maxSpeed, hardnessType);
-//   }
-
-//   for( const auto& position : m_explosions )
-//   {
-//     std::ranges::copy(level_explosion { position }, std::back_inserter(m_particles));
-//     m_playEvents.SetEvent(play_events::event_type::explosion, true);
-//   }
-
-//   m_explosions.clear();
-
-//   for( const auto& position : m_impacts )
-//   {
-//     m_particles.emplace_back(particle::type::impact, position, VELOCITY_2F { 0, 0 }, 0.5f);
-//   }
-
-//   m_impacts.clear();
-
-//   if( m_playerShip->EmitThrustParticle() )
-//   {
-//     auto thrustAngle = direct2d::RotateAngle(m_playerShip->Angle(), 180);
-//     auto thrustPosition = direct2d::CalculatePosition(m_playerShip->Position(), thrustAngle, 20);
-//     auto thrustVelocity = direct2d::CombineVelocities(m_playerShip->Velocity(), direct2d::CalculateVelocity(50.0f, thrustAngle));
-//     m_particles.emplace_back(particle::type::thrust, thrustPosition, thrustVelocity, 0.5f);
-//   }
-// }
 
 auto level_container::CreateNewObjects(float interval) -> void
 {
@@ -277,40 +190,40 @@ auto level_container::CreateNewObjects(float interval) -> void
 
 auto level_container::GetTargettedObject() -> targetted_object_type
 {
-  m_targetPosition = direct2d::CalculatePosition(m_playerShip->Position(), m_playerShip->Angle(), 100);
+//   m_targetPosition = direct2d::CalculatePosition(m_playerShip->Position(), m_playerShip->Angle(), 100);
 
-  if( m_targetPosition )
-  {
-    auto targetAngle = m_playerShip->Angle();
+//   if( m_targetPosition )
+//   {
+//     auto targetAngle = m_playerShip->Angle();
 
-    constexpr auto angleSpan = 20.0f;
+//     constexpr auto angleSpan = 20.0f;
 
-    mine_object* nearestMine = std::accumulate(std::begin(m_mines), std::end(m_mines), static_cast<mine_object*>(nullptr), [this, targetAngle](auto* nearest, auto& next) -> mine_object*
-    {
-      auto mineAngle = direct2d::GetAngleBetweenPoints(m_playerShip->Position(), next->Position());
-      auto angleDifference = direct2d::GetAngleDifference(targetAngle, mineAngle);
-      if( angleDifference < -angleSpan || angleDifference > angleSpan ) return nearest;
-      else return nearest ? &GetNearestToTarget(*nearest, next) : &next;
-    });
+//     mine_object* nearestMine = std::accumulate(std::begin(m_mines), std::end(m_mines), static_cast<mine_object*>(nullptr), [this, targetAngle](auto* nearest, auto& next) -> mine_object*
+//     {
+//       auto mineAngle = direct2d::GetAngleBetweenPoints(m_playerShip->Position(), next->Position());
+//       auto angleDifference = direct2d::GetAngleDifference(targetAngle, mineAngle);
+//       if( angleDifference < -angleSpan || angleDifference > angleSpan ) return nearest;
+//       else return nearest ? &GetNearestToTarget(*nearest, next) : &next;
+//     });
 
-#ifdef TARGETS_ARE_TARGETS
-    auto activeTargets = std::ranges::views::filter(m_targets, [](const auto& target) { return target->IsActivated() ? false : true; });
+// #ifdef TARGETS_ARE_TARGETS
+//     auto activeTargets = std::ranges::views::filter(m_targets, [](const auto& target) { return target->IsActivated() ? false : true; });
 
-    target_object* nearestTarget = std::accumulate(std::ranges::begin(activeTargets), std::ranges::end(activeTargets), static_cast<target_object*>(nullptr), [this, targetAngle](auto* nearest, auto& next) -> target_object*
-    {
-      auto mineAngle = direct2d::GetAngleBetweenPoints(m_playerShip->Position(), next->Position());
-      auto angleDifference = direct2d::GetAngleDifference(targetAngle, mineAngle);
-      if( angleDifference < -angleSpan || angleDifference > angleSpan ) return nearest;
-      return nearest ? &GetNearestToTarget(*nearest, next) : &next;
-    });
-#else
-  target_object* nearestTarget = nullptr;
-#endif
+//     target_object* nearestTarget = std::accumulate(std::ranges::begin(activeTargets), std::ranges::end(activeTargets), static_cast<target_object*>(nullptr), [this, targetAngle](auto* nearest, auto& next) -> target_object*
+//     {
+//       auto mineAngle = direct2d::GetAngleBetweenPoints(m_playerShip->Position(), next->Position());
+//       auto angleDifference = direct2d::GetAngleDifference(targetAngle, mineAngle);
+//       if( angleDifference < -angleSpan || angleDifference > angleSpan ) return nearest;
+//       return nearest ? &GetNearestToTarget(*nearest, next) : &next;
+//     });
+// #else
+//   target_object* nearestTarget = nullptr;
+// #endif
 
-    return GetNearestObject(nearestMine, nearestTarget, m_maxTargetRange);
-  }
-  else
-  {
+//     return GetNearestObject(nearestMine, nearestTarget, m_maxTargetRange);
+//   }
+//   else
+//   {
     return std::nullopt;
-  }
+//   }
 }
--- source/level_container/level_container.h
+++ source/level_container/level_container.h
@@ -15,30 +15,24 @@ class level_container
 
 public:
 
-  using target_collection = std::vector<target_object>;
-  using duct_fan_collection = std::vector<duct_fan_object>;
-  using asteroid_collection = std::vector<asteroid_object>;
-  using mine_collection = std::list<mine_object>;
-  using bullet_collection = std::list<homing_bullet>;
-  using particle_collection = std::list<particle>;
-  using explosion_collection = std::vector<D2D1_POINT_2F>;
-  using impact_collection = std::vector<D2D1_POINT_2F>;
-
   using static_object_collection = std::vector<dynamic_object<default_object>>;
   using moving_object_collection = std::list<dynamic_object<default_object>>;
+
+  using bullet_collection = std::list<homing_bullet>;
+  using particle_collection = std::list<particle>;
+
+  using explosion_collection = std::vector<D2D1_POINT_2F>;
+  using impact_collection = std::vector<D2D1_POINT_2F>;
 
   level_container(std::ranges::input_range auto&& points, play_events playEvents, std::shared_ptr<game_score> gameScore);
   level_container(const level_container& levelContainer) = delete;
 
   auto AddTargets(std::ranges::input_range auto&& positions) -> void;
-  // auto AddAsteroids(std::ranges::input_range auto&& asteroids) -> void;
-  // auto AddDuctFans(std::ranges::input_range auto&& asteroids) -> void;
   auto SetPlayerActive(bool value) -> void;
 
   auto Update(float interval, D2D1_RECT_F viewRect) -> void;
   auto Render(D2D1_RECT_F viewRect) const -> void;
 
-  [[nodiscard]] auto Targets() const -> const target_collection&;
   [[nodiscard]] auto PlayerPosition() const -> std::optional<POINT_2F>;
   [[nodiscard]] auto PlayerAngle() const -> float;
   [[nodiscard]] auto PlayerHasThrusterOn() const -> bool;
@@ -59,7 +53,7 @@ public:
   auto TargetActivated() -> void;
   auto MineDestroyed() -> void;
   auto SetPlayEvent(auto&&...args) -> void;
-  auto SetPlayerPosition(std::optional<POINT_2F> value) -> void;
+  auto UpdatePlayer(player_ship playerShip) -> void;
 
 private:
 
@@ -67,13 +61,12 @@ private:
   auto ValidateObjectPointers() -> void;
   auto RemoveDestroyedObjects() -> void;
   auto DoCollisions() -> void;
-  // auto CreateNewObjects(float interval) -> void;
   auto CreateNewObjects(float interval) -> void;
   auto GetTargettedObject() -> targetted_object_type;
-  auto GetNearestObject(auto* object1, auto* object2, float maxRange) const -> targetted_object_type;
-  auto GetNearestObject(auto* object1, auto* object2) const -> std::tuple<targetted_object_type, float>;
-  auto GetNearestToTarget(auto& mine1, auto& mine2) const -> auto&;
-  auto DistanceFromTarget(auto&& object) const -> float;
+  // auto GetNearestObject(auto* object1, auto* object2, float maxRange) const -> targetted_object_type;
+  // auto GetNearestObject(auto* object1, auto* object2) const -> std::tuple<targetted_object_type, float>;
+  // auto GetNearestToTarget(auto& mine1, auto& mine2) const -> auto&;
+  // auto DistanceFromTarget(auto&& object) const -> float;
   static [[nodiscard]] auto ConvertFireModeToDamageMode(player_ship::fire_mode fireMode) -> std::optional<bullet::damage_mode>;
 
 private:
@@ -82,12 +75,8 @@ private:
 
   blank_object m_boundary;
   play_events m_playEvents;
-  dynamic_object<player_ship> m_playerShip { level_geometries::PlayerShipGeometry(), D2D1_POINT_2F { 0, 0 } };
-  target_collection m_targets;
-  mine_collection m_mines;
-  duct_fan_collection m_ductFans;
+  std::optional<player_ship> m_playerShip;
   bullet_collection m_bullets;
-  asteroid_collection m_asteroids;
   particle_collection m_particles;
 
   static_object_collection m_staticObjects;
@@ -96,9 +85,8 @@ private:
   explosion_collection m_explosions;
   impact_collection m_impacts;
 
-  std::optional<POINT_2F> m_playerPosition;
-
   int m_activatedTargetCount { 0 };
+  
   targetted_object_type m_targettedObject;
   std::optional<D2D1_POINT_2F> m_targetPosition;
 
@@ -113,33 +101,17 @@ private:
 level_container::level_container(std::ranges::input_range auto&& points, play_events playEvents, std::shared_ptr<game_score> gameScore) : 
   m_boundary { points }, m_playEvents { playEvents }, m_gameScore { gameScore }
 {
-    m_movingObjects.emplace_back(level_geometries::PlayerShipGeometry(), std::in_place_type<player_ship>, POINT_2F { 0 , 0 });
+  m_playerShip = player_ship { POINT_2F { 0 , 0 } };
+  m_movingObjects.emplace_back(level_geometries::PlayerShipGeometry(), std::in_place_type<player_ship>, POINT_2F { 0 , 0 });
 }
 
 auto level_container::AddTargets(std::ranges::input_range auto&& positions) -> void
 {
   std::ranges::for_each(positions, [this](const auto& position)
   {
-    // m_targets.emplace_back(level_geometries::TargetGeometry(), position, 5.0f);
     m_staticObjects.emplace_back(level_geometries::TargetGeometry(), std::in_place_type<level_target>, position, 5.0f);
   });
 }
-
-// auto level_container::AddDuctFans(std::ranges::input_range auto&& positions) -> void
-// {
-//   std::ranges::for_each(positions, [this](const auto& position)
-//   {
-//     m_ductFans.emplace_back(level_geometries::DuctFanGeometry(), position, 30.0f);
-//   });
-// }
-
-// auto level_container::AddAsteroids(std::ranges::input_range auto&& positions) -> void
-// {
-//   std::ranges::for_each(positions, [this](const auto& position)
-//   {
-//     m_asteroids.emplace_back(level_geometries::AsteroidGeometry(), position, 200.0f, 200.0f);
-//   });
-// }
 
 inline [[nodiscard]] auto level_container::PlayerAngle() const -> float
 {
@@ -151,15 +123,9 @@ inline [[nodiscard]] auto level_container::PlayerShields() const -> const player
   return m_playerShip->ShieldStatus();  
 }
 
-inline[[nodiscard]] auto level_container::Targets() const -> const target_collection&
-{
-  return m_targets;
-}
-
 inline [[nodiscard]] auto level_container::PlayerPosition() const -> std::optional<POINT_2F>
 {
-  // return m_playerShip->Position();
-  return m_playerPosition;
+  return m_playerShip->Position();
 }
 
 inline [[nodiscard]] auto level_container::PlayerHasThrusterOn() const -> bool
@@ -242,48 +208,48 @@ auto level_container::SetPlayEvent(auto&&...args) -> void
   m_playEvents.SetEvent(std::forward<decltype(args)>(args)...);
 }
 
-inline auto level_container::SetPlayerPosition(std::optional<POINT_2F> value) -> void
+inline auto level_container::UpdatePlayer(player_ship playerShip) -> void
 {
-  m_playerPosition = value;
+  m_playerShip = playerShip;
 }
 
-auto level_container::GetNearestObject(auto* object1, auto* object2, float maxRange) const -> targetted_object_type
-{
-  auto [nearestObject, distance] = GetNearestObject(object1, object2);
-  return distance < maxRange ? nearestObject : std::nullopt;
-}
+// auto level_container::GetNearestObject(auto* object1, auto* object2, float maxRange) const -> targetted_object_type
+// {
+//   auto [nearestObject, distance] = GetNearestObject(object1, object2);
+//   return distance < maxRange ? nearestObject : std::nullopt;
+// }
 
-auto level_container::GetNearestObject(auto* object1, auto* object2) const -> std::tuple<targetted_object_type, float>
-{
-  if( object1 && object2 )
-  {
-    auto distance1 = DistanceFromTarget(*object1);
-    auto distance2 = DistanceFromTarget(*object2);
-    return distance2 < distance1 ? std::tuple<targetted_object_type, float> { targetted_object { object2 }, distance2 } : std::tuple<targetted_object_type, float> { targetted_object { object1 }, distance1 };
-  }
-  else if( object1 )
-  {
-    return { object1, DistanceFromTarget(*object1) };
-  }
-  else if( object2 )
-  {
-    return { object2, DistanceFromTarget(*object2) };
-  }
-  else
-  {
-    return { std::nullopt, 0.0f };
-  }
-}
+// auto level_container::GetNearestObject(auto* object1, auto* object2) const -> std::tuple<targetted_object_type, float>
+// {
+//   if( object1 && object2 )
+//   {
+//     auto distance1 = DistanceFromTarget(*object1);
+//     auto distance2 = DistanceFromTarget(*object2);
+//     return distance2 < distance1 ? std::tuple<targetted_object_type, float> { targetted_object { object2 }, distance2 } : std::tuple<targetted_object_type, float> { targetted_object { object1 }, distance1 };
+//   }
+//   else if( object1 )
+//   {
+//     return { object1, DistanceFromTarget(*object1) };
+//   }
+//   else if( object2 )
+//   {
+//     return { object2, DistanceFromTarget(*object2) };
+//   }
+//   else
+//   {
+//     return { std::nullopt, 0.0f };
+//   }
+// }
 
-auto level_container::GetNearestToTarget(auto& object1, auto& object2) const -> auto&
-{
-  return DistanceFromTarget(object2) < DistanceFromTarget(object1) ? object2 : object1;
-}
+// auto level_container::GetNearestToTarget(auto& object1, auto& object2) const -> auto&
+// {
+//   return DistanceFromTarget(object2) < DistanceFromTarget(object1) ? object2 : object1;
+// }
 
-auto level_container::DistanceFromTarget(auto&& object) const -> float
-{
-  return direct2d::GetDistanceBetweenPoints(*m_targetPosition, object->Position());
-}
+// auto level_container::DistanceFromTarget(auto&& object) const -> float
+// {
+//   return direct2d::GetDistanceBetweenPoints(*m_targetPosition, object->Position());
+// }
 
 inline [[nodiscard]] auto level_container::ConvertFireModeToDamageMode(player_ship::fire_mode fireMode) -> std::optional<bullet::damage_mode>
 {
