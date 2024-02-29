f2fd5b49b5347ff4482d4bd82bf559ce99d8b655
Mathew Coburn <mathew.coburn@gmail.com>
2024-02-29 20:48:19 +0000

    mines smaller

--- source/level_container/objects/mine.h
+++ source/level_container/objects/mine.h
@@ -11,7 +11,7 @@ public:
 
   mine(POINT_2F position, float thrust, float maxSpeed, type type, POINT_2F targetPosition);
 
-  [[nodiscard]] auto Scale() const -> SCALE_2F { return { 1.0f, 1.0f }; };
+  [[nodiscard]] auto Scale() const -> SCALE_2F { return { 0.5f, 0.5f }; };
   [[nodiscard]] auto Angle() const -> float;
   [[nodiscard]] auto Position() const -> POINT_2F;
   [[nodiscard]] auto Destroyed() const -> bool;
