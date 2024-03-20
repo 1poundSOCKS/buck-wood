610ee4e01933b6f226a5d1881483846c4f00051d
1poundSOCKS <mathew.coburn@gmail.com>
2024-03-20 07:29:36 +0000

    tidy up

--- source/level_data/game_level_data_loader.h
+++ source/level_data/game_level_data_loader.h
@@ -39,7 +39,6 @@ auto game_level_data_loader::LoadLevel(auto&&...args) -> std::unique_ptr<level_c
   std::unique_ptr<level_container> levelContainer = std::make_unique<level_container>(level_container::level_type::arena, m_levelIndex, m_demoLevel.BoundaryPoints(), m_demoLevel.PlayerPosition(), std::forward<decltype(args)>(args)...);
 
   levelContainer->CreatePortal(POINT_2F {0, 0});
-  // levelContainer->CreatePlayer(demoLevel.PlayerPosition());
 
   m_targetsToCreate = 1;
   m_levelTimer = reload_timer { 3.0f };
