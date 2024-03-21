2c83f1f42bf6249a49604646610cfae4346283e6
1poundSOCKS <mathew.coburn@gmail.com>
2024-03-21 11:14:58 +0000

    cylic rendering of targets working

--- source/renderers/level_target_renderer.h
+++ source/renderers/level_target_renderer.h
@@ -15,6 +15,7 @@ public:
 
 private:
 
+  constexpr static cyclic_interval m_cyclicIntervalReversal { 4.0f };
   constexpr static cyclic_interval m_cyclicInterval { 2.0f };
   color_scale_brushes m_targetBrushes { color_scale { screen_render_brush_red.Get(), screen_render_brush_black.Get(), 10 } };
 
@@ -22,6 +23,8 @@ private:
 
 inline auto level_target_renderer::Write(const level_target& object, ID2D1Geometry* geometry) const -> void
 {
-  float fillBrushSelection = ( 1.0f - m_cyclicInterval.get(object.Age()) ) * 0.8f + 0.2f;
+  auto reversedCycle = static_cast<int>(m_cyclicIntervalReversal.get(object.Age()) * 2.0f) ? true : false;
+  auto cyclicAge = reversedCycle ? m_cyclicInterval.get(object.Age()) : 1.0f - m_cyclicInterval.get(object.Age());
+  float fillBrushSelection = cyclicAge * 0.8f + 0.2f;
   geometry_renderer::Write(geometry, m_targetBrushes[fillBrushSelection].get(), m_targetBrushes[0.0f].get(), 10);
 }
