bc922898e10cdc8cda4e444d89a0a16ecc8da2f3
Mathew Coburn <mathew.coburn@gmail.com>
2024-03-23 09:56:25 +0000

    refactor

--- source/level_container/objects/portal.h
+++ source/level_container/objects/portal.h
@@ -1,57 +1,15 @@
 #pragma once
 
 #include "framework.h"
+#include "base_object.h"
 
-class portal
+class portal : public base_object
 {
 
 public:
 
-  portal(POINT_2F position) : m_position { position }
+  portal(POINT_2F position) : base_object { position, { 0, 0 }, { 150.0f, 150.0f }, 0 }
   {
   }
-
-  constexpr [[nodiscard]] auto Scale() const -> SCALE_2F
-  {
-    return m_scale;
-  }
-
-  constexpr [[nodiscard]] auto Angle() const -> float
-  {
-    return m_angle;
-  }
-
-  [[nodiscard]] auto Position() const -> POINT_2F
-  {
-    return m_position;
-  }
-
-  constexpr [[nodiscard]] auto Destroyed() const -> bool
-  {
-    return m_destroyed;
-  }
-
-  auto Update(float interval) -> void
-  {
-    m_age += interval;
-  }
-
-  auto Destroy() -> void
-  {
-  }
-
-  [[nodiscard]] auto Age() const noexcept -> float
-  {
-    return m_age;
-  }
-
-private:
-
-  POINT_2F m_position;
-  constexpr static SCALE_2F m_scale { 150.0f, 150.0f };
-  constexpr static float m_angle { 0 };
-  constexpr static bool m_destroyed { false };
-  constexpr static VELOCITY_2F m_velocity { 0, 0 };
-  float m_age { 0 };
 
 };
