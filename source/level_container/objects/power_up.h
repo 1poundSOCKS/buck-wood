543235f81b10ee84e1101fa04df13b6369362a38
Mathew Coburn <mathew.coburn@gmail.com>
2024-03-23 09:51:14 +0000

    refactor

--- source/level_container/objects/power_up.h
+++ source/level_container/objects/power_up.h
@@ -1,43 +1,15 @@
 #pragma once
 
-class power_up
+#include "framework.h"
+#include "base_object.h"
+
+class power_up : public base_object
 {
 
 public:
 
-  power_up(POINT_2F position, VELOCITY_2F velocity) : m_position { position }, m_velocity { velocity }
+  power_up(POINT_2F position, VELOCITY_2F velocity) : base_object { position , velocity, { 20.0f, 20.0f }, 0 }
   {
   }
-
-  constexpr [[nodiscard]] auto Scale() const -> SCALE_2F { return m_scale; };
-  constexpr [[nodiscard]] auto Angle() const -> float { return m_angle; }
-
-  [[nodiscard]] auto Position() const -> POINT_2F
-  {
-    return m_position;
-  }
-
-  [[nodiscard]] auto Destroyed() const -> bool
-  {
-    return m_destroyed;
-  }
-
-  auto Update(float interval) -> void
-  {
-    m_position = direct2d::CalculatePosition(m_position, m_velocity, interval);
-  }
-
-  auto Destroy() -> void
-  {
-    m_destroyed = true;
-  }
-
-private:
-
-  POINT_2F m_position;
-  VELOCITY_2F m_velocity;
-  constexpr static SCALE_2F m_scale { 20.0f, 20.0f };
-  constexpr static float m_angle { 0 };
-  bool m_destroyed { false };
 
 };
