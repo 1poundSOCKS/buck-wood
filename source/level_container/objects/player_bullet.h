1be2b8907b12f514ce66ba1810188f2ebcc5e149
Mathew Coburn <mathew.coburn@gmail.com>
2024-03-23 07:58:45 +0000

    refactor

--- source/level_container/objects/player_bullet.h
+++ source/level_container/objects/player_bullet.h
@@ -1,44 +1,16 @@
 #pragma once
 
 #include "framework.h"
+#include "base_object.h"
 
-class player_bullet
+class player_bullet : public base_object
 {
 
 public:
 
-  player_bullet(POINT_2F position, VELOCITY_2F velocity, int damage) : m_position { position }, m_velocity { velocity }, m_angle { direct2d::CalculateDirection(velocity) }, m_damage { damage }
+  player_bullet(POINT_2F position, VELOCITY_2F velocity, int damage) : base_object { position, velocity, { 1.5f, 1.5f }, direct2d::CalculateDirection(velocity) },
+    m_damage { damage }
   {
-  }
-
-  [[nodiscard]] auto Scale() const -> SCALE_2F
-  {
-    return m_scale;
-  }
-
-  [[nodiscard]] auto Angle() const -> float
-  {
-    return m_angle;
-  }
-
-  [[nodiscard]] auto Position() const -> POINT_2F
-  {
-    return m_position;
-  }
-
-  [[nodiscard]] auto Destroyed() const -> bool
-  {
-    return m_destroyed;
-  }
-
-  auto Update(float interval) -> void
-  {
-    m_position = direct2d::CalculatePosition(m_position, m_velocity, interval);
-  }
-
-  auto Destroy() -> void
-  {
-    m_destroyed = true;
   }
 
   [[nodiscard]] auto Damage()
@@ -48,11 +20,6 @@ public:
 
 private:
 
-  POINT_2F m_position;
-  SCALE_2F m_scale { 1.5f, 1.5f };
-  float m_angle;
-  bool m_destroyed { false };
-  VELOCITY_2F m_velocity;
   int m_damage;
 
 };
