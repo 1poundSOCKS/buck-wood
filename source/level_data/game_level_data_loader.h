29a41f90b7320aaab1bca80981401371e9227091
Mathew Coburn <mathew.coburn@gmail.com>
2024-03-23 10:16:21 +0000

    updates to game level loader

--- source/level_data/game_level_data_loader.h
+++ source/level_data/game_level_data_loader.h
@@ -11,12 +11,13 @@ class game_level_data_loader
 
 public:
 
-  enum class state_type { inactive, active };
+  enum class state_type { starting, active, finished };
 
   game_level_data_loader() = default;
 
   auto LoadLevel(auto&&...args) -> std::unique_ptr<level_container>;
   auto UpdateLevel(level_container* levelContainer, float interval) -> void;
+
   [[nodiscard]] auto MoreLevels() const -> bool;
   [[nodiscard]] auto NextLevel() -> bool;
   [[nodiscard]] auto CurrentLevel() const -> int;
@@ -24,10 +25,15 @@ public:
 
 private:
 
-  state_type m_state { state_type::inactive };
+  auto UpdateActiveLevel(level_container* levelContainer, float interval) -> void;
+
+private:
+
+  state_type m_state { state_type::starting };
   int m_levelIndex { -1 };
   inline static int m_levelCount { 99 };
-  reload_timer m_levelTimer { 5.0f };
+  reload_timer m_levelTimer { 3.0f };
+  reload_timer m_targetTimer { 3.0f };
   reload_timer m_powerUpTimer { 7.0f };
   bool m_updateComplete { false };
   int m_targetsToCreate { 1 };
@@ -47,35 +53,34 @@ auto game_level_data_loader::LoadLevel(auto&&...args) -> std::unique_ptr<level_c
   m_levelTimer = reload_timer { 3.0f };
   m_targetsToCreate = m_levelIndex + 1;
 
+  m_levelTimer = { 3.0f };
+  m_targetTimer = { 3.0f };
+  m_powerUpTimer = { 7.0f };
+
+  m_state = state_type::starting;
+
   return levelContainer;
 }
 
 inline auto game_level_data_loader::UpdateLevel(level_container* levelContainer, float interval) -> void
 {
-  if( m_levelTimer.Update(interval) )
+  switch( m_state )
   {
-    switch( m_state )
-    {
-      case state_type::inactive:
-        levelContainer->CreatePlayer(m_demoLevel.PlayerPosition());
+    case state_type::starting:
+      if( m_levelTimer.Update(interval) )
+      {
         m_state = state_type::active;
-        break;
+        levelContainer->CreatePlayer(m_demoLevel.PlayerPosition());
+      }
+      break;
 
-      case state_type::active:
-        if( m_targetsToCreate > 0 )
-        {
-          levelContainer->CreateTarget( POINT_2F { 0, 0 }, 4.0f, 10);    
-          --m_targetsToCreate;
-        }
+    case state_type::active:
+      UpdateActiveLevel(levelContainer, interval);
+      m_state = MoreUpdates() ? state_type::active : state_type::finished;
+      break;
 
-      default:
-        break;
-    }
-  }
-
-  if( m_state == state_type::active && m_powerUpTimer.Update(interval) )
-  {
-    levelContainer->CreatePowerUp( POINT_2F { 0, 0 }, m_randomVelocity.get() );
+    default:
+      break;
   }
 }
 
@@ -86,7 +91,6 @@ inline [[nodiscard]] auto game_level_data_loader::MoreLevels() const -> bool
 
 inline [[nodiscard]] auto game_level_data_loader::NextLevel() -> bool
 {
-  m_state = state_type::inactive;
   return ++m_levelIndex < m_levelCount;
 }
 
@@ -98,4 +102,18 @@ inline [[nodiscard]] auto game_level_data_loader::CurrentLevel() const -> int
 inline [[nodiscard]] auto game_level_data_loader::MoreUpdates() const -> bool
 {
   return m_targetsToCreate > 0 ? true : false;
+}
+
+inline auto game_level_data_loader::UpdateActiveLevel(level_container* levelContainer, float interval) -> void
+{
+  if( m_targetTimer.Update(interval) && m_targetsToCreate > 0 )
+  {
+    levelContainer->CreateTarget( POINT_2F { 0, 0 }, 4.0f, 10);    
+    --m_targetsToCreate;
+  }
+
+  if( m_powerUpTimer.Update(interval) )
+  {
+    levelContainer->CreatePowerUp( POINT_2F { 0, 0 }, m_randomVelocity.get() );
+  }
 }
