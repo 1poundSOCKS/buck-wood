fa34e33e63bdac2411c2f56200d2654921a37f1b
Mathew Coburn <mathew.coburn@gmail.com>
2024-04-15 13:22:16 +0100

    collision work for proton

--- source/level_container/collisions/geometry_containment.h
+++ source/level_container/collisions/geometry_containment.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "game_settings.h"
+
 class geometry_containment
 {
 
@@ -10,6 +12,34 @@ public:
   }
 
   auto operator()(ID2D1Geometry* containmentGeometry, auto&& containedObject, auto&& callable) -> void
+  {
+    switch( game_settings::collisionDetectionType() )
+    {
+      case game_settings::collision_detection_type::direct2d:
+        if( CheckDirect2D(containmentGeometry, containedObject) )
+        {
+          std::lock_guard<std::mutex> guard(m_mutex);
+          
+          if( !containedObject.Object().Destroyed() )
+          {
+            callable(containedObject.Object());
+          }
+        }
+        break;
+      case game_settings::collision_detection_type::basic:
+        if( !direct2d::CheckOverlap(direct2d::GetGeometryBounds(containmentGeometry), containedObject.GeometryBounds()) )
+        {
+          std::lock_guard<std::mutex> guard(m_mutex);
+          if( !containedObject.Object().Destroyed() )
+          {
+            callable(containedObject.Object());
+          }
+        }
+        break;
+    }
+  }
+
+  auto CheckDirect2D(ID2D1Geometry* containmentGeometry, auto&& containedObject) -> bool
   {
     D2D1_GEOMETRY_RELATION relation = D2D1_GEOMETRY_RELATION_UNKNOWN;
     HRESULT hr = containmentGeometry->CompareWithGeometry(containedObject.Geometry(), D2D1::Matrix3x2F::Identity(), &relation);
@@ -26,15 +56,17 @@ public:
       }
     }
 
-    if( !contained )
-    {
-      std::lock_guard<std::mutex> guard(m_mutex);
+    // if( !contained )
+    // {
+    //   std::lock_guard<std::mutex> guard(m_mutex);
       
-      if( !containedObject.Object().Destroyed() )
-      {
-        callable(containedObject.Object());
-      }
-    }
+    //   if( !containedObject.Object().Destroyed() )
+    //   {
+    //     callable(containedObject.Object());
+    //   }
+    // }
+
+    return contained;
   }
 
   auto operator()(ID2D1Geometry* containmentGeometry, std::ranges::input_range auto&& objectCollection, auto&& callable) -> void
