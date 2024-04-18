c160eb2b7887d5876d216e9f4776671a16ba2ac4
Mathew Coburn <mathew.coburn@gmail.com>
2024-04-18 09:47:06 +0100

    enemy movement updates

--- source/level_container/objects/enemy_type_2.cpp
+++ source/level_container/objects/enemy_type_2.cpp
@@ -1,8 +1,8 @@
 #include "pch.h"
 #include "enemy_type_2.h"
 
-enemy_type_2::enemy_type_2(POINT_2F position, float reloadTime, int hitpoints) : 
-  enemy_object { position, { 1.5, 1.5 }, 0, 3 }, m_reloadTimer { reloadTime }
+enemy_type_2::enemy_type_2(POINT_2F position, int hitpoints, float speed, float reloadTime) : 
+  enemy_object { position, { 1.5, 1.5 }, 0, hitpoints }, m_speed { speed }, m_reloadTimer { reloadTime }
 {
 }
 
@@ -10,28 +10,14 @@ auto enemy_type_2::Update(float interval) -> void
 {
   base_object::Update(interval);
 
-  switch( m_state )
-  {
-    case state_type::moving:
-    {
-      m_destination = m_destination ? m_destination : std::optional<POINT_2F>({static_cast<float>(m_positionDist(pseudo_random_generator::get())) * 100, static_cast<float>(m_positionDist(pseudo_random_generator::get())) * 100});
-      bool atDestination = m_destination && MoveTowards(m_speed * interval, *m_destination);
-      m_destination = atDestination ? std::optional<POINT_2F>({static_cast<float>(m_positionDist(pseudo_random_generator::get())) * 100, static_cast<float>(m_positionDist(pseudo_random_generator::get())) * 100})  : m_destination;
-      m_actionCount += atDestination ? 1 : 0;
-      bool stateChange = m_actionCount < m_movesBeforeStateChange ? false : true;
-      m_state = stateChange ? state_type::shooting : m_state;
-      m_actionCount = stateChange ? 0 : m_actionCount;
-      break;
-    }
-    case state_type::shooting:
-    {
-      m_reloaded = m_reloadTimer.Update(interval);
-      m_actionCount += m_reloaded ? 1 : 0;
-      bool stateChange = m_actionCount > m_shotsBeforeStateChange ? true : false;
-      m_state = stateChange ? state_type::moving : m_state;
-      m_actionCount = stateChange ? 0 : m_actionCount;
-      m_reloaded = stateChange ? false : m_reloaded;
-      break;
-    }
-  }
+  m_destination = m_destination ? m_destination : NewDestination();
+  m_destination = MoveTowards(m_speed * interval, *m_destination) ? NewDestination() : m_destination;
+  m_reloaded = m_reloadTimer.Update(interval);
+}
+
+auto enemy_type_2::NewDestination() -> POINT_2F
+{
+  static std::uniform_int_distribution<int> directionDist { 0, 7 };
+  auto direction = directionDist(pseudo_random_generator::get()) * 45;
+  return direct2d::CalculatePosition(m_position, static_cast<float>(direction), 100);
 }
--- source/level_container/objects/enemy_type_2.h
+++ source/level_container/objects/enemy_type_2.h
@@ -8,38 +8,26 @@ class enemy_type_2 : public enemy_object
 
 public:
 
-  enum class state_type { moving, shooting };
-
-  enemy_type_2(POINT_2F position);
-  enemy_type_2(POINT_2F position, float reloadTime, int hitpoints);
+  enemy_type_2(POINT_2F position, int hitpoints, float speed, float reloadTime);
 
   auto Update(float interval) -> void;
 
-  [[nodiscard]] auto State() const -> state_type;
   [[nodiscard]] auto Destination() const -> std::optional<POINT_2F>;
   [[nodiscard]] auto CanShootAt(POINT_2F position) const -> bool;
   [[nodiscard]] auto Reloaded() const -> bool;
 
 private:
 
-  state_type m_state { state_type::moving };
-  constexpr static int m_movesBeforeStateChange { 3 };
-  constexpr static float m_speed = 300;
-  inline static std::uniform_int_distribution<int> m_positionDist { -10, 10 };
+  [[nodiscard]] auto NewDestination() -> POINT_2F;
 
+private:
+
+  float m_speed;
   std::optional<POINT_2F> m_destination;
   reload_timer m_reloadTimer;
   bool m_reloaded { false };
-  constexpr static int m_shotsBeforeStateChange { 5 };
-
-  int m_actionCount { 0 };
 
 };
-
-inline [[nodiscard]] auto enemy_type_2::State() const -> state_type
-{
-  return m_state;
-}
 
 inline [[nodiscard]] auto enemy_type_2::Destination() const -> std::optional<POINT_2F>
 {
