2eacf842b800a1aaefbd4015f5e614f1335510a4
Mathew Coburn <mathew.coburn@gmail.com>
2024-04-22 15:56:12 +0100

    refactor

--- source/level_data/demo_level.cpp
+++ source/level_data/demo_level.cpp
@@ -23,10 +23,6 @@ demo_level::demo_level()
   auto pointIterator = std::begin(m_boundaryPoints);
   std::advance(pointIterator, 1);
   std::ranges::copy(area2, std::inserter(m_boundaryPoints, pointIterator));
-  // m_boundaryPoints.insert(pointIterator, {static_cast<float>(-2 * m_cellSize), static_cast<float>(-4 * m_cellSize)});
-  // m_boundaryPoints.insert(pointIterator, {static_cast<float>(-2 * m_cellSize), static_cast<float>(-8 * m_cellSize)});
-  // m_boundaryPoints.insert(pointIterator, {static_cast<float>(2 * m_cellSize), static_cast<float>(-8 * m_cellSize)});
-  // m_boundaryPoints.insert(pointIterator, {static_cast<float>(2 * m_cellSize), static_cast<float>(-4 * m_cellSize)});
 
   m_boundaryGeometry = direct2d::CreatePathGeometry(d2d_factory::get_raw(), m_boundaryPoints, D2D1_FIGURE_END_CLOSED);
 }
