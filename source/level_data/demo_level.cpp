304165dd1fe616c621cc28d42e7bfb618e466869
Mathew Coburn <mathew.coburn@gmail.com>
2024-04-22 16:02:53 +0100

    refactor

--- source/level_data/demo_level.cpp
+++ source/level_data/demo_level.cpp
@@ -5,24 +5,27 @@
 demo_level::demo_level()
 {
   auto area1 = std::array {
-    POINT_2F { static_cast<float>(-4 * m_cellSize), static_cast<float>(-4 * m_cellSize) },
-    POINT_2F { static_cast<float>(4 * m_cellSize), static_cast<float>(-4 * m_cellSize) },
-    POINT_2F { static_cast<float>(4 * m_cellSize), static_cast<float>(4 * m_cellSize) },
-    POINT_2F { static_cast<float>(-4 * m_cellSize), static_cast<float>(4 * m_cellSize) }
+    POINT_2F { static_cast<float>(-4), static_cast<float>(-4) },
+    POINT_2F { static_cast<float>(4), static_cast<float>(-4) },
+    POINT_2F { static_cast<float>(4), static_cast<float>(4) },
+    POINT_2F { static_cast<float>(-4), static_cast<float>(4) }
   };
 
   auto area2 = std::array {
-    POINT_2F {static_cast<float>(-2 * m_cellSize), static_cast<float>(-4 * m_cellSize) },
-    POINT_2F {static_cast<float>(-2 * m_cellSize), static_cast<float>(-8 * m_cellSize) },
-    POINT_2F {static_cast<float>(2 * m_cellSize), static_cast<float>(-8 * m_cellSize) },
-    POINT_2F {static_cast<float>(2 * m_cellSize), static_cast<float>(-4 * m_cellSize) }
+    POINT_2F {static_cast<float>(-2), static_cast<float>(-4) },
+    POINT_2F {static_cast<float>(-2), static_cast<float>(-8) },
+    POINT_2F {static_cast<float>(2), static_cast<float>(-8) },
+    POINT_2F {static_cast<float>(2), static_cast<float>(-4) }
   };
 
-  std::ranges::copy(area1, std::back_inserter(m_boundaryPoints));
+  auto area1Transform = std::ranges::views::transform(area1, [this](auto point) { return POINT_2F { static_cast<float>(point.x * m_cellSize), static_cast<float>(point.y * m_cellSize) }; } );
+  auto area2Transform = std::ranges::views::transform(area2, [this](auto point) { return POINT_2F { static_cast<float>(point.x * m_cellSize), static_cast<float>(point.y * m_cellSize) }; } );
+
+  std::ranges::copy(area1Transform, std::back_inserter(m_boundaryPoints));
 
   auto pointIterator = std::begin(m_boundaryPoints);
   std::advance(pointIterator, 1);
-  std::ranges::copy(area2, std::inserter(m_boundaryPoints, pointIterator));
+  std::ranges::copy(area2Transform, std::inserter(m_boundaryPoints, pointIterator));
 
   m_boundaryGeometry = direct2d::CreatePathGeometry(d2d_factory::get_raw(), m_boundaryPoints, D2D1_FIGURE_END_CLOSED);
 }
