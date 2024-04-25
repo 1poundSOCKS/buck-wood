8dc3742d62aae7bcd83a5360e054e9b140864e6e
Mathew Coburn <mathew.coburn@gmail.com>
2024-04-25 13:52:08 +0100

    cell collisions improved

--- source/level_container/collisions/cell_collision_tests.h
+++ source/level_container/collisions/cell_collision_tests.h
@@ -29,6 +29,48 @@ auto cell_collision_tests::operator()(const level_cell_collection& cells, auto&&
 {
     std::lock_guard<std::mutex> guard(m_mutex);
 
+    auto walls = std::ranges::views::filter(cells.Get(), [](const auto& cellEntry)
+    {
+      const auto& [key, value] = cellEntry;
+      return value.Type() == valid_cell::cell_type::wall;
+    });
+
+    // auto wallGeometries = std::ranges::views::transform(walls, [](const auto& cellEntry)
+    // {
+    //   const auto& [key, value] = cellEntry;
+    //   return value.Geometry();
+    // });
+
+    for( const auto& cellEntry : walls )
+    {
+      const auto& [key, value] = cellEntry;
+
+      D2D1_GEOMETRY_RELATION relation = D2D1_GEOMETRY_RELATION_UNKNOWN;
+      HRESULT hr = object.Geometry()->CompareWithGeometry(value.Geometry().get(), D2D1::Matrix3x2F::Identity(), &relation);
+
+      bool collided = false;
+
+      if( SUCCEEDED(hr) )
+      {
+        switch( relation )
+        {
+          case D2D1_GEOMETRY_RELATION_IS_CONTAINED:
+          case D2D1_GEOMETRY_RELATION_CONTAINS:
+          case D2D1_GEOMETRY_RELATION_OVERLAP:
+            collided = true;
+            break;
+        }
+      }
+
+      if( collided )
+      {
+        if( !object.Object().Destroyed() )
+        {
+          callable(object.Object());
+        }
+      }
+    }
+
     auto cellType = cells.CellType(object.Object().Position());
 
     switch( cellType )
