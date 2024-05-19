c24405965b661c47ba586779c9e00da5afc6ad2c
1poundSOCKS <mathew.coburn@gmail.com>
2024-05-19 07:56:11 +0100

    refactor

--- source/level_data/level_data_translator.h
+++ source/level_data/level_data_translator.h
@@ -2,6 +2,7 @@
 
 #include "level_base.h"
 #include "level_cell_data_translator.h"
+#include "level_object_data_translator.h"
 
 class level_data_translator
 {
@@ -13,11 +14,8 @@ public:
 
 private:
 
-  static [[nodiscard]] auto ItemType(char cellData) -> level_item_type;
-
-private:
-
   level_cell_data_translator m_cellDataTranslator;
+  level_object_data_translator m_objectDataTranslator;
 
 };
 
@@ -31,25 +29,8 @@ auto level_data_translator::EnumerateCells(const level_base* levelData, auto&& v
 
 auto level_data_translator::EnumerateItems(const level_base* levelData, auto&& visitor) const -> void
 {
-  levelData->EnumerateItems([&visitor](size_t column, size_t row, char itemType)
+  levelData->EnumerateItems([this,&visitor](size_t column, size_t row, char itemType)
   {
-    visitor(column, row, ItemType(itemType));
+    visitor(column, row, m_objectDataTranslator(itemType));
   });
-}
-
-inline [[nodiscard]] auto level_data_translator::ItemType(char cellData) -> level_item_type
-{
-  switch( cellData )
-  {
-    case 'P':
-      return level_item_type::portal;
-    case '1':
-      return level_item_type::enemy_type_one;
-    case '2':
-      return level_item_type::enemy_type_two;
-    case '3':
-      return level_item_type::enemy_type_three;
-    default:
-      return level_item_type::none;
-  }
 }
