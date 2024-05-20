ec3ff93c5a6511d20f1af9bb7cdb6847f64276ed
Mathew Coburn <mathew.coburn@gmail.com>
2024-05-20 07:26:57 +0100

    refactor

--- source/level_data/level_types.h
+++ source/level_data/level_types.h
@@ -1,4 +1,4 @@
 #pragma once
 
-enum class level_cell_type { none, wall, floor, exit, entry };
+enum class level_cell_type { none, wall, floor, exit };
 enum class level_item_type { none, portal, enemy_type_one, enemy_type_two, enemy_type_three };
