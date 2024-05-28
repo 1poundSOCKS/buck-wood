4bc550457ced4e073eca43dd96aa71e1d4f6bb1a
Mathew Coburn <mathew.coburn@gmail.com>
2024-05-28 20:09:54 +0100

    refactor

--- source/geometry/level_geometries.cpp
+++ source/geometry/level_geometries.cpp
@@ -1,6 +1,21 @@
 #include "pch.h"
 #include "level_geometries.h"
 
+auto level_geometries::create() -> void
+{
+  destroy();
+  m_instance = new level_geometries();
+}
+
+auto level_geometries::destroy() -> void
+{
+  if( m_instance )
+  {
+    delete m_instance;
+    m_instance = nullptr;
+  }
+}
+
 level_geometries::level_geometries()
 {
   m_rectangleGeometry = direct2d::CreatePathGeometry(d2d_factory::get_raw(), level_geometry_functions::GetRectangleGeometryData(), D2D1_FIGURE_END_CLOSED);
@@ -15,6 +30,69 @@ level_geometries::level_geometries()
   m_portal = ScaledGeometry(object_type::portal, { 200, 200 });
   m_powerUp = ScaledGeometry(object_type::power_up, { 60, 60 });
   LoadHudTargetGeometries(std::back_inserter(m_hudTargetGeometries));
+}
+
+auto level_geometries::Get(object_type objectType) -> winrt::com_ptr<ID2D1Geometry>
+{
+  switch( objectType )
+  {
+    case object_type::player:
+      return m_player;
+    case object_type::player_bullet:
+      return m_playerBullet;
+    case object_type::enemy_1:
+      return m_enemy1;
+    case object_type::enemy_2:
+      return m_enemy2;
+    case object_type::enemy_3:
+      return m_enemy3;
+    case object_type::enemy_bullet_1:
+      return m_enemyBullet1;
+    case object_type::portal:
+      return m_portal;
+    case object_type::power_up:
+      return m_powerUp;
+    case object_type::wall:
+      return m_rectangleGeometry;
+    default:
+      return m_rectangleGeometry;
+  }
+}
+
+auto level_geometries::GetBase(object_type objectType) -> winrt::com_ptr<ID2D1Geometry>
+{
+  switch( objectType )
+  {
+    case object_type::player:
+      return m_targetGeometry;
+    case object_type::player_bullet:
+      return m_targetGeometry;
+    case object_type::enemy_1:
+      return m_targetGeometry;
+    case object_type::enemy_2:
+      return m_targetGeometry;
+    case object_type::enemy_3:
+      return m_rectangleGeometry;
+    case object_type::enemy_bullet_1:
+      return m_mineGeometry;
+    case object_type::portal:
+      return m_targetGeometry;
+    case object_type::power_up:
+      return m_targetGeometry;
+    case object_type::wall:
+      return m_rectangleGeometry;
+    default:
+      return m_rectangleGeometry;
+  }
+}
+
+auto level_geometries::LoadHudTargetGeometries(auto&& geometryInserter) -> void
+{
+  auto topLeftGeometry = direct2d::CreatePathGeometry(d2d_factory::get_raw(), level_geometry_functions::GetHudTargetTopLeftGeometryData(), D2D1_FIGURE_END_OPEN);
+  geometryInserter = topLeftGeometry;
+  geometryInserter = direct2d::CreateTransformedGeometry(d2d_factory::get_raw(), topLeftGeometry.get(), D2D1::Matrix3x2F::Rotation(90));
+  geometryInserter = direct2d::CreateTransformedGeometry(d2d_factory::get_raw(), topLeftGeometry.get(), D2D1::Matrix3x2F::Rotation(180));
+  geometryInserter = direct2d::CreateTransformedGeometry(d2d_factory::get_raw(), topLeftGeometry.get(), D2D1::Matrix3x2F::Rotation(270));
 }
 
 auto level_geometries::ScaledGeometry(object_type objectType, SIZE_F geometrySize) -> winrt::com_ptr<ID2D1Geometry>
--- source/geometry/level_geometries.h
+++ source/geometry/level_geometries.h
@@ -8,117 +8,25 @@ class level_geometries
 
 public:
 
-  static auto create() -> void
-  {
-    destroy();
-    m_instance = new level_geometries();
-  }
+  static auto create() -> void;
+  static auto destroy() -> void;
 
-  static auto destroy() -> void
-  {
-    if( m_instance )
-    {
-      delete m_instance;
-      m_instance = nullptr;
-    }
-  }
-
-  static [[nodiscard]] auto get(const default_object& defaultObject) -> winrt::com_ptr<ID2D1Geometry>
-  {
-    return std::visit([](const auto& object) { return get(object); }, defaultObject.Get());
-  }
-
-  static [[nodiscard]] auto get(auto&& object) -> winrt::com_ptr<ID2D1Geometry>
-  {
-    auto objectType = level_objects::Type(object);
-    return get(objectType);
-  }
-
-  static [[nodiscard]] auto get(object_type objectType) -> winrt::com_ptr<ID2D1Geometry>
-  {
-    return m_instance->Get(objectType);
-  }
-
-  static [[nodiscard]] auto RectangleGeometry() -> winrt::com_ptr<ID2D1Geometry>
-  {
-    return m_instance->m_rectangleGeometry;
-  }
-
-  static [[nodiscard]] auto HudTargetGeometries() -> const std::vector<winrt::com_ptr<ID2D1Geometry>>&
-  {
-    return m_instance->m_hudTargetGeometries;
-  }
-
+  static [[nodiscard]] auto get(const default_object& defaultObject) -> winrt::com_ptr<ID2D1Geometry>;
+  static [[nodiscard]] auto get(auto&& object) -> winrt::com_ptr<ID2D1Geometry>;
+  static [[nodiscard]] auto get(object_type objectType) -> winrt::com_ptr<ID2D1Geometry>;
+  static [[nodiscard]] auto RectangleGeometry() -> winrt::com_ptr<ID2D1Geometry>;
+  static [[nodiscard]] auto HudTargetGeometries() -> const std::vector<winrt::com_ptr<ID2D1Geometry>>&;
 
 private:
 
   level_geometries();
 
-  [[nodiscard]] auto Get(object_type objectType) -> winrt::com_ptr<ID2D1Geometry>
-  {
-    switch( objectType )
-    {
-      case object_type::player:
-        return m_player;
-      case object_type::player_bullet:
-        return m_playerBullet;
-      case object_type::enemy_1:
-        return m_enemy1;
-      case object_type::enemy_2:
-        return m_enemy2;
-      case object_type::enemy_3:
-        return m_enemy3;
-      case object_type::enemy_bullet_1:
-        return m_enemyBullet1;
-      case object_type::portal:
-        return m_portal;
-      case object_type::power_up:
-        return m_powerUp;
-      case object_type::wall:
-        return m_rectangleGeometry;
-      default:
-        return m_rectangleGeometry;
-    }
-  }
-
+  [[nodiscard]] auto Get(object_type objectType) -> winrt::com_ptr<ID2D1Geometry>;
+  [[nodiscard]] auto GetBase(object_type objectType) -> winrt::com_ptr<ID2D1Geometry>;
   [[nodiscard]] auto ScaledGeometry(object_type objectType, SIZE_F geometrySize) -> winrt::com_ptr<ID2D1Geometry>;
+
   static [[nodiscard]] auto Scale(ID2D1Geometry* geometry, SIZE_F size) -> SCALE_2F;
-
-  [[nodiscard]] auto GetBase(object_type objectType) -> winrt::com_ptr<ID2D1Geometry>
-  {
-    switch( objectType )
-    {
-      case object_type::player:
-        return m_targetGeometry;
-      case object_type::player_bullet:
-        return m_targetGeometry;
-      case object_type::enemy_1:
-        return m_targetGeometry;
-      case object_type::enemy_2:
-        return m_targetGeometry;
-      case object_type::enemy_3:
-        return m_rectangleGeometry;
-      case object_type::enemy_bullet_1:
-        return m_mineGeometry;
-      case object_type::portal:
-        return m_targetGeometry;
-      case object_type::power_up:
-        return m_targetGeometry;
-      case object_type::wall:
-        return m_rectangleGeometry;
-      default:
-        return m_rectangleGeometry;
-    }
-  }
-
-  static [[nodiscard]] auto LoadHudTargetGeometries(auto&& geometryInserter) -> void
-  {
-    auto topLeftGeometry = direct2d::CreatePathGeometry(d2d_factory::get_raw(), level_geometry_functions::GetHudTargetTopLeftGeometryData(), D2D1_FIGURE_END_OPEN);
-    geometryInserter = topLeftGeometry;
-    geometryInserter = direct2d::CreateTransformedGeometry(d2d_factory::get_raw(), topLeftGeometry.get(), D2D1::Matrix3x2F::Rotation(90));
-    geometryInserter = direct2d::CreateTransformedGeometry(d2d_factory::get_raw(), topLeftGeometry.get(), D2D1::Matrix3x2F::Rotation(180));
-    geometryInserter = direct2d::CreateTransformedGeometry(d2d_factory::get_raw(), topLeftGeometry.get(), D2D1::Matrix3x2F::Rotation(270));
-  }
+  static [[nodiscard]] auto LoadHudTargetGeometries(auto&& geometryInserter) -> void;
 
 private:
 
@@ -139,3 +47,29 @@ private:
   winrt::com_ptr<ID2D1Geometry> m_powerUp;
 
 };
+
+inline [[nodiscard]] auto level_geometries::get(const default_object& defaultObject) -> winrt::com_ptr<ID2D1Geometry>
+{
+  return std::visit([](const auto& object) { return get(object); }, defaultObject.Get());
+}
+
+[[nodiscard]] auto level_geometries::get(auto&& object) -> winrt::com_ptr<ID2D1Geometry>
+{
+  auto objectType = level_objects::Type(object);
+  return get(objectType);
+}
+
+inline [[nodiscard]] auto level_geometries::get(object_type objectType) -> winrt::com_ptr<ID2D1Geometry>
+{
+  return m_instance->Get(objectType);
+}
+
+inline [[nodiscard]] auto level_geometries::RectangleGeometry() -> winrt::com_ptr<ID2D1Geometry>
+{
+  return m_instance->m_rectangleGeometry;
+}
+
+inline [[nodiscard]] auto level_geometries::HudTargetGeometries() -> const std::vector<winrt::com_ptr<ID2D1Geometry>>&
+{
+  return m_instance->m_hudTargetGeometries;
+}
