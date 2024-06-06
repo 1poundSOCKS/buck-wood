bbfddf4fadae626c80a10b0e09d9f12570915800
1poundSOCKS <mathew.coburn@gmail.com>
2024-06-06 12:47:31 +0100

    crash bug fixed

--- source/level_container/adjacent_floor_cells.cpp
+++ source/level_container/adjacent_floor_cells.cpp
@@ -3,11 +3,28 @@
 
 adjacent_floor_cells::adjacent_floor_cells(const level_cell_collection &cells, cell_id cellId) : m_cells { cells }, m_cellId { cellId }
 {
-  m_adjacentCellIds.reserve(4);
-  m_adjacentCellIds.emplace_back(m_cellId.Get(cell_id::relative_position::above));
-  m_adjacentCellIds.emplace_back(m_cellId.Get(cell_id::relative_position::right));
-  m_adjacentCellIds.emplace_back(m_cellId.Get(cell_id::relative_position::below));
-  m_adjacentCellIds.emplace_back(m_cellId.Get(cell_id::relative_position::left));
+  auto aboveId = m_cellId.Get(cell_id::relative_position::above);
+  auto rightId = m_cellId.Get(cell_id::relative_position::right);
+  auto belowId = m_cellId.Get(cell_id::relative_position::below);
+  auto leftId = m_cellId.Get(cell_id::relative_position::left);
+
+  bool aboveIsFloor = m_cells.IsTypeOf(aboveId, level_cell_type::floor);
+  bool rightIsFloor = m_cells.IsTypeOf(rightId, level_cell_type::floor);
+  bool belowIsFloor = m_cells.IsTypeOf(belowId, level_cell_type::floor);
+  bool leftIsFloor = m_cells.IsTypeOf(leftId, level_cell_type::floor);
+
+  auto adjacentFloorCellCount = 0;
+  adjacentFloorCellCount += aboveIsFloor ? 1 : 0;
+  adjacentFloorCellCount += rightIsFloor ? 1 : 0;
+  adjacentFloorCellCount += belowIsFloor ? 1 : 0;
+  adjacentFloorCellCount += leftIsFloor ? 1 : 0;
+
+  m_adjacentCellIds.reserve(adjacentFloorCellCount);
+
+  if( aboveIsFloor ) m_adjacentCellIds.emplace_back(m_cellId.Get(cell_id::relative_position::above));
+  if( rightIsFloor ) m_adjacentCellIds.emplace_back(m_cellId.Get(cell_id::relative_position::right));
+  if( belowIsFloor ) m_adjacentCellIds.emplace_back(m_cellId.Get(cell_id::relative_position::below));
+  if( leftIsFloor ) m_adjacentCellIds.emplace_back(m_cellId.Get(cell_id::relative_position::left));
 }
 
 auto adjacent_floor_cells::Count() const noexcept -> size_t
