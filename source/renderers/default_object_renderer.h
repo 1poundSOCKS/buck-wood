b0958d8e900ce880138464819bc18a292af3c740
1poundSOCKS <mathew.coburn@gmail.com>
2024-06-18 16:36:12 +0100

    render changes

--- source/renderers/default_object_renderer.h
+++ source/renderers/default_object_renderer.h
@@ -31,7 +31,7 @@ private:
 
   geometry_renderer m_backgroundObjectRenderer { screen_render_brush_very_dark_grey.CreateBrush() };
   color_scale_brushes m_targetBrushes { color_scale { screen_render_brush_red.Get(), screen_render_brush_black.Get(), 10 } };
-  geometry_renderer m_floorCellRenderer { screen_render_brush_black.CreateBrush() };
+  geometry_renderer m_floorCellRenderer { screen_render_brush_grey.CreateBrush() };
   player_ship_renderer m_playerShipRenderer;
   flashing_geometry_renderer m_playerMissileRenderer { color_scale { screen_render_brush_yellow.Get(), screen_render_brush_black.Get(), 20 } };
   flashing_geometry_renderer m_enemyType1_renderer { color_scale { screen_render_brush_blue.Get(), screen_render_brush_black.Get(), 20 }, 10 };
