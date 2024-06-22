dbe34f24ce251ceac291b4e1a6839db6d8a35773
1poundSOCKS <mathew.coburn@gmail.com>
2024-06-22 09:33:44 +0100

    refactor

--- source/level_container/level_container.cpp
+++ source/level_container/level_container.cpp
@@ -156,19 +156,16 @@ auto level_container::UpdateObject(player_ship &object, float interval) -> void
 
 auto level_container::UpdateObject(enemy_type_1 &object, float interval) -> void
 {
-  // object.SetCells(m_cells);
   object.Update(interval, *m_cells);
 }
 
 auto level_container::UpdateObject(enemy_type_2 &object, float interval) -> void
 {
-  // object.SetCells(m_cells);
   object.Update(interval, *m_cells);
 }
 
 auto level_container::UpdateObject(enemy_type_3 &object, float interval) -> void
 {
-  // object.SetCells(m_cells);
   object.Update(interval);
 }
 
