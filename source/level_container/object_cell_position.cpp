4ad7516448b23e13021760f4d503713550a1374d
1poundSOCKS <mathew.coburn@gmail.com>
2024-08-03 07:07:57 +0100

    refactor

--- source/level_container/object_cell_position.cpp
+++ source/level_container/object_cell_position.cpp
@@ -84,24 +84,5 @@ auto object_cell_position::MoveDirection(float direction) noexcept -> move_direc
 
 auto object_cell_position::MoveDirection(float x, float y) noexcept -> move_direction
 {
-  // auto angleToTarget = static_cast<int>(direct2d::GetAngle(POINT_2F { x, y }));
-
-  // auto adjustedAngleToTarget = angleToTarget + 45;
-  // auto normalizedAngleToTarget = adjustedAngleToTarget > 359 ? adjustedAngleToTarget - 360 : adjustedAngleToTarget;
-  // auto segmentedAngleToTarget = normalizedAngleToTarget / 90;
-
-  // switch( segmentedAngleToTarget )
-  // {
-  //   case 0:
-  //     return object_cell_position::move_direction::up;
-  //   case 1:
-  //     return object_cell_position::move_direction::right;
-  //   case 2:
-  //     return object_cell_position::move_direction::down;
-  //   case 3:
-  //     return object_cell_position::move_direction::left;
-  //   default:
-  //     return object_cell_position::move_direction::none;
-  // }
   return MoveDirection(direct2d::GetAngle(POINT_2F { x, y }));
 }
