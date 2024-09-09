34bc7f28b7364cc36eaa99798333465b824cb046
1poundSOCKS <mathew.coburn@gmail.com>
2024-09-09 13:14:41 +0100

    pixel geometry work...

--- source/level_container/objects/player_ship_state.h
+++ source/level_container/objects/player_ship_state.h
@@ -134,6 +134,7 @@ inline auto player_ship_state::ShootAngle() const noexcept -> std::optional<floa
 inline auto player_ship_state::SetShootAngle(float value) noexcept -> void
 {
   m_shootAngle = value;
+  m_angle = value;
 }
 
 inline auto player_ship_state::ResetShootAngle() noexcept -> void
