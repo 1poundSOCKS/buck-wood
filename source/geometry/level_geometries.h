a4d18529626a6ed7c9da1b8df8715cc0daeae604
1poundSOCKS <mathew.coburn@gmail.com>
2024-09-17 17:24:10 +0100

    player geometry update

--- source/geometry/level_geometries.h
+++ source/geometry/level_geometries.h
@@ -34,9 +34,11 @@ private:
   inline static level_geometries* m_instance { nullptr };
 
   inline static auto m_playerPixelImage = std::array {
-    std::string { " 0 " },
-    std::string { "000" },
-    std::string { " 0 " }
+    std::string { "  0  0  " },
+    std::string { " 00  00 " },
+    std::string { " 000000 " },
+    std::string { " 000000 " },
+    std::string { "000  000" }
   };
 
   inline static auto m_playerPixelData = std::array {
