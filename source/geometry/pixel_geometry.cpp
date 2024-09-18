d5cc29bda33e50315123f8681e5808d8f9fa937f
1poundSOCKS <mathew.coburn@gmail.com>
2024-09-18 09:05:44 +0100

    refactor

--- source/geometry/pixel_geometry.cpp
+++ source/geometry/pixel_geometry.cpp
@@ -7,3 +7,61 @@ pixel_geometry::operator winrt::com_ptr<ID2D1Geometry>() const noexcept
 {
   return m_geometry;
 }
+
+auto pixel_geometry::LoadGeometry(const pixel_id_lookup &pixelIdLookup, cell_size pixelSize) noexcept -> void
+{
+  std::map<POINT_2I, POINT_2I> pixelLines;
+
+  for( auto pixelId : pixelIdLookup )
+  {
+    auto leftId = pixelId.Get(cell_id::relative_position::left);
+    auto aboveId = pixelId.Get(cell_id::relative_position::above);
+    auto rightId = pixelId.Get(cell_id::relative_position::right);
+    auto belowId = pixelId.Get(cell_id::relative_position::below);
+
+    auto pixelPosition = pixelId.Position(1, 1);
+
+    if( !pixelIdLookup.contains(leftId) )
+    {
+      auto pixelLineStart = POINT_2I { pixelPosition.x, pixelPosition.y + 1 };
+      auto pixelLineEnd = POINT_2I { pixelPosition.x, pixelPosition.y };
+      pixelLines[pixelLineStart] = pixelLineEnd;
+    }
+
+    if( !pixelIdLookup.contains(aboveId) )
+    {
+      auto pixelLineStart = POINT_2I { pixelPosition.x, pixelPosition.y };
+      auto pixelLineEnd = POINT_2I { pixelPosition.x + 1, pixelPosition.y };
+      pixelLines[pixelLineStart] = pixelLineEnd;
+    }
+
+    if( !pixelIdLookup.contains(rightId) )
+    {
+      auto pixelLineStart = POINT_2I { pixelPosition.x + 1, pixelPosition.y };
+      auto pixelLineEnd = POINT_2I { pixelPosition.x + 1, pixelPosition.y + 1 };
+      pixelLines[pixelLineStart] = pixelLineEnd;
+    }
+
+    if( !pixelIdLookup.contains(belowId) )
+    {
+      auto pixelLineStart = POINT_2I { pixelPosition.x + 1, pixelPosition.y + 1 };
+      auto pixelLineEnd = POINT_2I { pixelPosition.x, pixelPosition.y + 1 };
+      pixelLines[pixelLineStart] = pixelLineEnd;
+    }
+  }
+
+  std::list<POINT_2F> points;
+
+  auto currentLine = std::begin(pixelLines);
+  
+  while( points.size() < pixelLines.size() )
+  {
+    const auto& [pixelLineStart, pixelLineEnd] = *currentLine;
+    auto pixelRect = pixelSize.CellRect({pixelLineStart.x, pixelLineStart.y});
+    auto pixelRectFloat = ToFloat(pixelRect);
+    points.emplace_back(pixelRectFloat.left, pixelRectFloat.top);
+    currentLine = pixelLines.find(pixelLineEnd);
+  }
+
+  m_geometry = direct2d::CreatePathGeometry(d2d_factory::get_raw(), points, D2D1_FIGURE_END_CLOSED);
+}
--- source/geometry/pixel_geometry.h
+++ source/geometry/pixel_geometry.h
@@ -17,6 +17,10 @@ public:
 
 private:
 
+  using pixel_id_lookup = std::set<cell_id>;
+
+  auto LoadGeometry(const pixel_id_lookup& pixelIdLookup, cell_size pixelSize) noexcept -> void;
+  
   static [[nodiscard]] auto NextPositionClockwise(cell_id::relative_position currentPosition) -> cell_id::relative_position;
   static [[nodiscard]] auto NextPositionAnticlockwise(cell_id::relative_position currentPosition) -> cell_id::relative_position;
   static [[nodiscard]] auto OppositePosition(cell_id::relative_position currentPosition) -> cell_id::relative_position;
@@ -34,62 +38,7 @@ pixel_geometry::pixel_geometry(std::ranges::input_range auto&& pixelIds, cell_si
 
 inline auto pixel_geometry::Load(std::ranges::input_range auto &&pixelIds, cell_size pixelSize) noexcept -> void
 {
-  std::set<cell_id> pixelIdLookup, borderPixelIdLookup;
-
+  pixel_id_lookup pixelIdLookup;
   std::ranges::copy(pixelIds, std::inserter(pixelIdLookup, std::begin(pixelIdLookup)));
-
-  std::map<POINT_2I, POINT_2I> pixelLines;
-
-  for( auto pixelId : pixelIdLookup )
-  {
-    auto leftId = pixelId.Get(cell_id::relative_position::left);
-    auto aboveId = pixelId.Get(cell_id::relative_position::above);
-    auto rightId = pixelId.Get(cell_id::relative_position::right);
-    auto belowId = pixelId.Get(cell_id::relative_position::below);
-
-    auto pixelPosition = pixelId.Position(1, 1);
-
-    if( !pixelIdLookup.contains(leftId) )
-    {
-      auto pixelLineStart = POINT_2I { pixelPosition.x, pixelPosition.y + 1 };
-      auto pixelLineEnd = POINT_2I { pixelPosition.x, pixelPosition.y };
-      pixelLines[pixelLineStart] = pixelLineEnd;
-    }
-
-    if( !pixelIdLookup.contains(aboveId) )
-    {
-      auto pixelLineStart = POINT_2I { pixelPosition.x, pixelPosition.y };
-      auto pixelLineEnd = POINT_2I { pixelPosition.x + 1, pixelPosition.y };
-      pixelLines[pixelLineStart] = pixelLineEnd;
-    }
-
-    if( !pixelIdLookup.contains(rightId) )
-    {
-      auto pixelLineStart = POINT_2I { pixelPosition.x + 1, pixelPosition.y };
-      auto pixelLineEnd = POINT_2I { pixelPosition.x + 1, pixelPosition.y + 1 };
-      pixelLines[pixelLineStart] = pixelLineEnd;
-    }
-
-    if( !pixelIdLookup.contains(belowId) )
-    {
-      auto pixelLineStart = POINT_2I { pixelPosition.x + 1, pixelPosition.y + 1 };
-      auto pixelLineEnd = POINT_2I { pixelPosition.x, pixelPosition.y + 1 };
-      pixelLines[pixelLineStart] = pixelLineEnd;
-    }
-  }
-
-  std::list<POINT_2F> points;
-
-  auto currentLine = std::begin(pixelLines);
-  
-  while( points.size() < pixelLines.size() )
-  {
-    const auto& [pixelLineStart, pixelLineEnd] = *currentLine;
-    auto pixelRect = pixelSize.CellRect({pixelLineStart.x, pixelLineStart.y});
-    auto pixelRectFloat = ToFloat(pixelRect);
-    points.emplace_back(pixelRectFloat.left, pixelRectFloat.top);
-    currentLine = pixelLines.find(pixelLineEnd);
-  }
-
-  m_geometry = direct2d::CreatePathGeometry(d2d_factory::get_raw(), points, D2D1_FIGURE_END_CLOSED);
+  LoadGeometry(pixelIdLookup, pixelSize);
 }
