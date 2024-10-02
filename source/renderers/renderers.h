fdfba81831d501bbd7ec5395b49493e98c7bacdc
1poundSOCKS <mathew.coburn@gmail.com>
2024-10-02 19:44:52 +0100

    portal rendering disabled

--- source/renderers/renderers.h
+++ source/renderers/renderers.h
@@ -54,7 +54,6 @@ private:
   auto Render(const energy_bar& energyBar) const -> void;
   auto Render(const level_container& levelContainer) const -> void;
   auto Write(const default_object& object) const -> void;
-  auto Write(const portal &object) const -> void;
   auto Write(level_cell_collection::column_def columnDef) const noexcept -> void;
   auto Write(level_cell_collection::row_def rowDef) const noexcept -> void;
   auto Write(const auto& object) const -> void;
@@ -176,11 +175,6 @@ inline auto renderer::Write(const default_object &object) const -> void
     auto energyBar = energy_bar { energyBarRect.Get(), *health };
     Render(energyBar);
   }
-}
-
-inline auto renderer::Write(const portal &object) const -> void
-{
-  m_portalRenderer.Write(object);
 }
 
 inline auto renderer::Write(level_cell_collection::column_def columnDef) const noexcept -> void
