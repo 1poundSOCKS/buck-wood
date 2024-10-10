16d295a217407b0cf5ee6e170d2263dc0f5312bb
1poundSOCKS <mathew.coburn@gmail.com>
2024-10-10 08:33:45 +0100

    camera follows player

--- source/play_screen/play_scene.cpp
+++ source/play_screen/play_scene.cpp
@@ -60,8 +60,8 @@ auto play_scene::Render() const -> void
 
 auto play_scene::CameraPosition() const -> camera_sequence::camera_position
 {
-  auto levelCentrePoint = m_playState->LevelCentrePoint();
-  return camera_sequence::camera_position { levelCentrePoint.x, levelCentrePoint.y, m_cameraZoom };
+  auto playerPosition = m_playState->LevelContainer().PlayerState().Position();
+  return camera_sequence::camera_position { playerPosition.x, playerPosition.y, m_cameraZoom };
 }
 
 auto play_scene::RenderLevelContainer() const -> void
