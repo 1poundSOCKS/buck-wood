ee8262adc8fc8154928d95c691108c95ed5f3fa4
1poundSOCKS <mathew.coburn@gmail.com>
2024-10-11 17:30:50 +0100

    refactor

--- source/level_data/game_world.cpp
+++ source/level_data/game_world.cpp
@@ -51,38 +51,44 @@ auto game_world::LoadLevel(int levelIndex, std::optional<cell_id> entryCell) con
 {
   auto levelContainer = std::make_unique<level_container>();
 
-  auto levelData = std::array { 
-      std::string { "                                     " },
-      std::string { "                                     " },
-      std::string { "      1                              " },
-      std::string { "                                     " },
-      std::string { "                                     " },
-      std::string { "                                     " },
-      std::string { "                         3           " },
-      std::string { "                                     " },
-      std::string { "                                     " },
-      std::string { "              P                      " },
-      std::string { "                                     " },
-      std::string { "                                     " },
-      std::string { "                                     " },
-      std::string { "                                     " },
-      std::string { "                                     " },
-      std::string { "         2                           " },
-      std::string { "                                     " },
-      std::string { "                                     " },
-      std::string { "                                     " },
-      std::string { "                                     " },
-      std::string { "                                     " },
-      std::string { "                3             2      " },
-      std::string { "                                     " },
-      std::string { "                                     " },
-      std::string { "                         1           " },
-      std::string { "          2                          " },
-      std::string { "                                     " }
+  static std::array levelData { 
+    std::string { "XX       XXXXXX      XX             XXX" },
+    std::string { "X       XX   XX                       X" },
+    std::string { "X  1   XXX    X                        " },
+    std::string { "      XXXX                             " },
+    std::string { "                                      X" },
+    std::string { "                                     XX" },
+    std::string { "                         3      XXXXXXX" },
+    std::string { "                                   XXXX" },
+    std::string { "                                      X" },
+    std::string { "XX            P                       X" },
+    std::string { "XXXXXXXXXX                             " },
+    std::string { "XXXXXXXXXXXXX                          " },
+    std::string { "XXXXXXX                                " },
+    std::string { "XXX                                  XX" },
+    std::string { "                              XXXXXXXXX" },
+    std::string { "         2                         XXXX" },
+    std::string { "                                     XX" },
+    std::string { "                                       " },
+    std::string { "                                       " },
+    std::string { "                                       " },
+    std::string { "                                       " },
+    std::string { "                3             2        " },
+    std::string { "        X                              " },
+    std::string { "XX      X                              " },
+    std::string { "XXXX    X                1            X" },
+    std::string { "       XX    2    X                  XX" },
+    std::string { "       XX    2    X                  XX" },
+    std::string { "       XX    2    X                  XX" },
+    std::string { "       XX    2    X                  XX" },
+    std::string { "       XX    2    X                  XX" },
+    std::string { "       XX    2    X                  XX" },
+    std::string { "       XX    2    X                  XX" },
+    std::string { "       XX    2    X                  XX" },
+    std::string { "X     XXX         XX         XXXXXXXXXX" }
   };
   
   std::vector<pixel_geometry_loader::pixel_data> pixelData;
-
   pixel_geometry_loader::imageDataToPixelData(levelData, std::back_inserter(pixelData));
 
   for( auto&& pixel : pixelData )
