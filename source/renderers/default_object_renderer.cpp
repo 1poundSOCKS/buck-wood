bf5081cf95b0b579f76fafdf3d982768449fdb12
1poundSOCKS <mathew.coburn@gmail.com>
2024-10-26 18:22:02 +0100

    thrust render tweak

--- source/renderers/default_object_renderer.cpp
+++ source/renderers/default_object_renderer.cpp
@@ -48,15 +48,15 @@ auto default_object_renderer::Write(const enemy_ship& object, ID2D1Geometry* geo
 
 auto default_object_renderer::Write(const player_ship& object, ID2D1Geometry* geometry) const -> void
 {
-  m_playerShipRenderer.Write(object, geometry);
-
   auto playerThrust = level_geometries::getPlayerThrust();
   auto scalePlayerThrust = D2D1::Matrix3x2F::Scale(1.0f, 0.2f + object.State().BaseThrusterPower() * 1.5f);
-  auto movePlayerThrustDown = D2D1::Matrix3x2F::Translation(0.0f, 80.0f);
+  auto movePlayerThrustDown = D2D1::Matrix3x2F::Translation(0.0f, 70.0f);
   auto shiftedThrustGeometry = direct2d::CreateTransformedGeometry(d2d_factory::get_raw(), playerThrust.get(), scalePlayerThrust * movePlayerThrustDown);
   auto transform = geometric_object_transform { object };
   auto transformedThrustGeometry = direct2d::CreateTransformedGeometry(d2d_factory::get_raw(), shiftedThrustGeometry.get(), transform.Get());
   m_playerThrustRenderer.Write(transformedThrustGeometry.get());
+
+  m_playerShipRenderer.Write(object, geometry);
 }
 
 auto default_object_renderer::Write(const enemy_bullet& object, ID2D1Geometry* geometry) const -> void
