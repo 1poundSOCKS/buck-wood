47c6bd46badb63ce3a1a5da2dbc3f7158f41f9a8
1poundSOCKS <mathew.coburn@gmail.com>
2024-10-27 08:10:42 +0000

    refactor

--- source/level_container/objects/enemy_path.h
+++ source/level_container/objects/enemy_path.h
@@ -8,7 +8,6 @@ class enemy_path
 public:
 
   enemy_path(std::ranges::input_range auto&& points);
-  enemy_path(const enemy_path& enemyPath);
 
   [[nodiscard]] auto operator()(POINT_2F position, float speed, float interval) -> POINT_2F;
 
@@ -18,18 +17,13 @@ private:
 
   using container_type = std::vector<POINT_2F>;
   container_type m_points;
-  container_type::const_iterator m_destination;
+  container_type::size_type m_destination;
 
 };
 
-enemy_path::enemy_path(std::ranges::input_range auto&& points)
+enemy_path::enemy_path(std::ranges::input_range auto&& points) : m_destination { 0 }
 {
   std::ranges::copy(points, std::back_inserter(m_points));
-  m_destination = std::begin(m_points);
-}
-
-inline enemy_path::enemy_path(const enemy_path & enemyPath) : m_points { enemyPath.m_points }, m_destination { std::begin(m_points) }
-{
 }
 
 inline auto enemy_path::operator()(POINT_2F position, float speed, float interval) -> POINT_2F
@@ -39,16 +33,15 @@ inline auto enemy_path::operator()(POINT_2F position, float speed, float interva
 
 inline auto enemy_path::Move(POINT_2F position, float distance, float interval) -> POINT_2F
 {
-  auto destination = *m_destination;
+  auto destination = m_points[m_destination];
   auto distanceToDest = direct2d::GetDistanceBetweenPoints(position, destination);
 
   if( distance > distanceToDest && distanceToDest > 0.0f )
   {
     auto distanceLeft = distance - distanceToDest;
     auto intervalLeft = interval * distanceLeft / distance;
-    auto waypoint = *m_destination;
-    auto nextDestination = std::next(m_destination);
-    m_destination = ( nextDestination == std::end(m_points) ) ? std::begin(m_points) : nextDestination;
+    auto waypoint = m_points[m_destination];
+    m_destination = ( ++m_destination < m_points.size() ) ? m_destination : 0;
     return Move(waypoint, distanceLeft, intervalLeft);
   }
   else
