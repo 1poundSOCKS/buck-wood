1442d5bf7bbdfc04d10afd10a282bb3b095d4a25
1poundSOCKS <mathew.coburn@gmail.com>
2024-10-29 06:33:21 +0000

    refactor

--- source/level_container/objects/enemy_area.h
+++ source/level_container/objects/enemy_area.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "enemy_destination.h"
 #include "cell_id.h"
 #include "cell_size.h"
 
@@ -14,13 +15,10 @@ public:
 
 private:
 
-  [[nodiscard]] auto Move(POINT_2F position, float distance, float interval, auto&& getNextDestination) -> POINT_2F;
-  static [[nodiscard]] auto Move(POINT_2F position, POINT_2F destination, float distance) -> std::pair<POINT_2F,float>;
-
   using points_collection = std::vector<POINT_2F>;
   points_collection m_points;
   std::uniform_int_distribution<points_collection::size_type> m_pointIndex;
-  std::optional<POINT_2F> m_destination;
+  enemy_destination m_destination;
 
 };
 
@@ -32,39 +30,8 @@ inline enemy_area::enemy_area(std::ranges::input_range auto &&points) :
 
 inline auto enemy_area::operator()(POINT_2F position, float speed, float interval) -> POINT_2F
 {
-  return Move(position, speed, interval, [this](POINT_2F position) -> POINT_2F
+  return m_destination(position, speed, interval, [this](POINT_2F position) -> POINT_2F
   {
     return m_points[m_pointIndex(pseudo_random_generator::get())];
   });
-}
-
-inline auto enemy_area::Move(POINT_2F position, float speed, float interval, auto &&getNextDestination) -> POINT_2F
-{
-  m_destination = m_destination ? m_destination : getNextDestination(position);
-  auto distance = speed * interval;
-
-  while( distance > 0.0f )
-  {
-    auto&& [movePosition, remainingDistance] = Move(position, *m_destination, distance);
-    m_destination = IsEqual(movePosition, position) ? getNextDestination(position) : m_destination;
-    position = movePosition;
-    distance = remainingDistance;
-  }
-
-  return position;
-}
-
-inline auto enemy_area::Move(POINT_2F position, POINT_2F destination, float distance) -> std::pair<POINT_2F, float>
-{
-  auto distanceToDest = direct2d::GetDistanceBetweenPoints(position, destination);
-
-  if( distance > distanceToDest )
-  {
-    return { destination, distance - distanceToDest };
-  }
-  else
-  {
-    auto direction = direct2d::GetAngleBetweenPoints(position, destination);
-    return { direct2d::CalculatePosition(position, direction, distance), 0.0f };
-  }
 }
---
+++ source/level_container/objects/enemy_destination.h
@@ -0,0 +1,51 @@
+#pragma once
+
+#include "framework.h"
+
+class enemy_destination
+{
+
+public:
+
+  [[nodiscard]] auto operator()(POINT_2F position, float speed, float interval, auto &&getNextDestination) -> POINT_2F;
+
+private:
+
+  static [[nodiscard]] auto Move(POINT_2F position, POINT_2F destination, float distance) -> std::pair<POINT_2F,float>;
+
+private:
+
+  std::optional<POINT_2F> m_destination;
+
+};
+
+inline auto enemy_destination::operator()(POINT_2F position, float speed, float interval, auto &&getNextDestination) -> POINT_2F
+{
+  m_destination = m_destination ? m_destination : getNextDestination(position);
+  auto distance = speed * interval;
+
+  while( distance > 0.0f )
+  {
+    auto&& [movePosition, remainingDistance] = Move(position, *m_destination, distance);
+    m_destination = IsEqual(movePosition, position) ? getNextDestination(position) : m_destination;
+    position = movePosition;
+    distance = remainingDistance;
+  }
+
+  return position;
+}
+
+inline auto enemy_destination::Move(POINT_2F position, POINT_2F destination, float distance) -> std::pair<POINT_2F, float>
+{
+  auto distanceToDest = direct2d::GetDistanceBetweenPoints(position, destination);
+
+  if( distance > distanceToDest )
+  {
+    return { destination, distance - distanceToDest };
+  }
+  else
+  {
+    auto direction = direct2d::GetAngleBetweenPoints(position, destination);
+    return { direct2d::CalculatePosition(position, direction, distance), 0.0f };
+  }
+}
