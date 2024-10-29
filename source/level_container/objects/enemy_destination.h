2944afbc9ebb0c820a498197a7e48be108b6686e
1poundSOCKS <mathew.coburn@gmail.com>
2024-10-29 07:12:45 +0000

    refactor

--- source/level_container/objects/enemy_destination.h
+++ source/level_container/objects/enemy_destination.h
@@ -24,7 +24,7 @@ inline auto enemy_destination::operator()(POINT_2F position, float speed, float 
   m_destination = m_destination ? m_destination : getNextDestination();
   auto distance = speed * interval;
 
-  while( distance > 0.0f )
+  while( IsNotEqual(position, *m_destination) && distance > 0.0f )
   {
     auto&& [movePosition, remainingDistance] = Move(position, *m_destination, distance);
     m_destination = IsEqual(movePosition, position) ? getNextDestination() : m_destination;
@@ -32,31 +32,26 @@ inline auto enemy_destination::operator()(POINT_2F position, float speed, float 
     distance = remainingDistance;
   }
 
+  m_destination = IsEqual(position, *m_destination) ? getNextDestination() : m_destination;
+
   return position;
 }
 
 inline auto enemy_destination::Move(POINT_2F position, POINT_2F destination, float distance) -> std::pair<POINT_2F, float>
 {
-  if( IsEqual(position, destination) )
+  auto distanceToDest = direct2d::GetDistanceBetweenPoints(position, destination);
+
+  if( distanceToDest == 0.0f )
   {
-    return { position, 0.0f };
+    return { destination, 0.0f };
+  }
+  else if( distance > distanceToDest )
+  {
+    return { destination, distance - distanceToDest };
   }
   else
   {
-    auto distanceToDest = direct2d::GetDistanceBetweenPoints(position, destination);
-
-    if( distanceToDest == 0.0f )
-    {
-      return { destination, 0.0f };
-    }
-    else if( distance > distanceToDest )
-    {
-      return { destination, distance - distanceToDest };
-    }
-    else
-    {
-      auto direction = direct2d::GetAngleBetweenPoints(position, destination);
-      return { direct2d::CalculatePosition(position, direction, distance), 0.0f };
-    }    
+    auto direction = direct2d::GetAngleBetweenPoints(position, destination);
+    return { direct2d::CalculatePosition(position, direction, distance), 0.0f };
   }
 }
