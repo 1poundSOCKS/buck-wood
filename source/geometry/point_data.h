4a0ab23f6ddf5309d2fed34fbd035d53340b8478
1poundSOCKS <mathew.coburn@gmail.com>
2024-11-05 08:51:43 +0000

    refactor

--- source/geometry/point_data.h
+++ source/geometry/point_data.h
@@ -4,18 +4,28 @@
 
 namespace point_data
 {
+  auto CellsIdsToOrderedBoundaryPoints(std::ranges::input_range auto&& cellIds, int cellWidth, int cellHeight, auto boundaryInserter) -> void;
+  auto CellsIdsToBoundaryLines(std::ranges::input_range auto&& cellIds, auto lineInserter) -> void;
+  auto BoundaryLinesToOrderedBoundaryPoints(std::ranges::input_range auto&& lineData, int cellWidth, int cellHeight, auto boundaryInserter) -> void;
+  auto GetBounds(std::ranges::input_range auto &&pointData) -> RECT_F;
 
-  auto CellsIdsToOrderedBoundaryPoints(std::ranges::input_range auto&& emptyCellIds, int cellWidth, int cellHeight, auto boundaryInserter) -> void
+  auto CellsIdsToOrderedBoundaryPoints(std::ranges::input_range auto&& cellIds, int cellWidth, int cellHeight, auto boundaryInserter) -> void
   {
-    static_assert(std::is_same_v<std::ranges::range_value_t<decltype(emptyCellIds)>, POINT_2I>);
+    static_assert(std::is_same_v<std::ranges::range_value_t<decltype(cellIds)>, POINT_2I>);
+    
+    std::vector<std::pair<POINT_2I, POINT_2I>> lineData;
+    CellsIdsToBoundaryLines(cellIds, std::back_inserter(lineData));
+    BoundaryLinesToOrderedBoundaryPoints(lineData, cellWidth, cellHeight, boundaryInserter);
+  }
+
+  auto CellsIdsToBoundaryLines(std::ranges::input_range auto&& cellIds, auto lineInserter) -> void
+  {
+    static_assert(std::is_same_v<std::ranges::range_value_t<decltype(cellIds)>, POINT_2I>);
     
     std::set<POINT_2I> cellIdLookup;
-    std::ranges::copy(emptyCellIds, std::inserter(cellIdLookup, std::begin(cellIdLookup)));
+    std::ranges::copy(cellIds, std::inserter(cellIdLookup, std::begin(cellIdLookup)));
     
-    using line_data = std::pair<POINT_2I, POINT_2I>;
-    std::vector<line_data> lineData;
-
-    for( auto&& cellId : emptyCellIds )
+    for( auto&& cellId : cellIds )
     {
       auto&& [column, row] = cellId;
 
@@ -28,40 +38,41 @@ namespace point_data
       {
         auto pixelLineStart = POINT_2I { column, row + 1 };
         auto pixelLineEnd = POINT_2I { column, row };
-        lineData.emplace_back(pixelLineStart, pixelLineEnd);
+        lineInserter = std::pair<POINT_2I, POINT_2I>(pixelLineStart, pixelLineEnd);
       }
 
       if( !cellIdLookup.contains(aboveId) )
       {
         auto pixelLineStart = POINT_2I { column, row };
         auto pixelLineEnd = POINT_2I { column + 1, row };
-        lineData.emplace_back(pixelLineStart, pixelLineEnd);
+        lineInserter = std::pair<POINT_2I, POINT_2I>(pixelLineStart, pixelLineEnd);
       }
 
       if( !cellIdLookup.contains(rightId) )
       {
         auto pixelLineStart = POINT_2I { column + 1, row };
         auto pixelLineEnd = POINT_2I { column + 1, row + 1 };
-        lineData.emplace_back(pixelLineStart, pixelLineEnd);
+        lineInserter = std::pair<POINT_2I, POINT_2I>(pixelLineStart, pixelLineEnd);
       }
 
       if( !cellIdLookup.contains(belowId) )
       {
         auto pixelLineStart = POINT_2I { column + 1, row + 1 };
         auto pixelLineEnd = POINT_2I { column, row + 1 };
-        lineData.emplace_back(pixelLineStart, pixelLineEnd);
+        lineInserter = std::pair<POINT_2I, POINT_2I>(pixelLineStart, pixelLineEnd);
       }
     }
+  }
+
+  auto BoundaryLinesToOrderedBoundaryPoints(std::ranges::input_range auto&& lineData, int cellWidth, int cellHeight, auto boundaryInserter) -> void
+  {
+    static_assert(std::is_same_v<std::ranges::range_value_t<decltype(lineData)>, std::pair<POINT_2I, POINT_2I>>);
 
     std::map<POINT_2I, POINT_2I> pixelLines;
-    std::vector<POINT_2I> pointData;
-    std::vector<std::tuple<std::optional<POINT_2I>, POINT_2I, std::optional<POINT_2I>>> pointDataGroups;
-    std::vector<std::tuple<std::optional<POINT_2I>, POINT_2I, std::optional<POINT_2I>>> normalizedPointDataGroups;
-    
     std::ranges::copy(lineData, std::inserter(pixelLines, std::begin(pixelLines)));
 
+    std::vector<POINT_2I> pointData;
     auto currentLine = std::begin(pixelLines);
-
     while( currentLine != std::end(pixelLines) )
     {
       const auto& [pixelLineStart, pixelLineEnd] = *currentLine;
@@ -72,6 +83,7 @@ namespace point_data
       currentLine = pixelLines.find(pixelLineEnd);
     }
 
+    std::vector<std::tuple<std::optional<POINT_2I>, POINT_2I, std::optional<POINT_2I>>> pointDataGroups;
     for( auto pointIter = std::begin(pointData); pointIter != std::end(pointData); ++pointIter )
     {
       auto previousPoint = ( pointIter == std::begin(pointData) ? std::nullopt : std::optional<POINT_2I>(*std::prev(pointIter)) );
@@ -80,6 +92,7 @@ namespace point_data
       pointDataGroups.emplace_back(previousPoint, *pointIter, nextPoint);
     }
 
+    std::vector<std::tuple<std::optional<POINT_2I>, POINT_2I, std::optional<POINT_2I>>> normalizedPointDataGroups;
     std::ranges::copy_if(pointDataGroups, std::back_inserter(normalizedPointDataGroups), [](auto&& pointDataGroup)
     {
       auto&& [prev, current, next] = pointDataGroup;
@@ -98,6 +111,8 @@ namespace point_data
 
   auto GetBounds(std::ranges::input_range auto &&pointData) -> RECT_F
   {
+    static_assert(std::is_same_v<std::ranges::range_value_t<decltype(pointData)>, POINT_2F>);
+
     return std::accumulate(std::begin(pointData), std::end(pointData), RECT_F { 0.0f, 0.0f, 0.0f, 0.0f }, [](RECT_F bounds, POINT_2F point)
     {
       bounds.left = std::min(bounds.left, point.x);
